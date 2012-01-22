da631ce4366295311586a5f91b7e100fa662a3a8
psychi <psychicphysical+github.com@googlemail.com>
2012-01-22 14:05:41 +0900

    psyq::StlAllocatorのmemory境界値を、allocateで決定するように変更。

--- StlAllocator.hpp
+++ StlAllocator.hpp
@@ -3,24 +3,21 @@
 
 namespace psyq
 {
-	template< typename, std::size_t > class StlAllocator;
+	template< typename > class StlAllocator;
 }
 
 //ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
 /** @brief std::allocator互換のmemory割当子。
     @tparam t_value_type 確保するinstanceの型。
-    @tparam t_alignment  確保するinstanceのbyte単位の境界値。
  */
-template< typename t_value_type, std::size_t t_alignment = sizeof(void*) >
+template< typename t_value_type >
 class psyq::StlAllocator:
 	public std::allocator< t_value_type >
 {
-	typedef psyq::StlAllocator< t_value_type, t_alignment > This;
+	typedef psyq::StlAllocator< t_value_type > This;
 	typedef std::allocator< t_value_type > Super;
 
 	public:
-	enum{ alignment = t_alignment };
-
 	//-------------------------------------------------------------------------
 	/** @brief default-allocatorを使うmemory割当子を構築。
 	 */
@@ -43,9 +40,9 @@ class psyq::StlAllocator:
 
 	/** @param[in] 代入元のmemory割当子。
 	 */
-	template< typename t_other_type, std::size_t t_other_alinment >
+	template< typename t_other_type >
 	StlAllocator(
-		psyq::StlAllocator< t_other_type, t_other_alinment > const& i_allocator):
+		psyq::StlAllocator< t_other_type > const& i_allocator):
 	Super(i_allocator),
 	allocator(i_allocator.get_allocator())
 	{
@@ -67,9 +64,9 @@ class psyq::StlAllocator:
 	/** @brief 代入演算子だが、実際に代入は行われない。
 	    代入元と等値でない場合はassertする。
 	 */
-	template< typename t_other_type, std::size_t t_other_alinment >
+	template< typename t_other_type >
 	This& operator=(
-		psyq::StlAllocator< t_other_type, t_other_alinment > const& i_right)
+		psyq::StlAllocator< t_other_type > const& i_right)
 	{
 		// 等値であることを確認。
 		PSYQ_ASSERT(*this == i_right);
@@ -83,9 +80,20 @@ class psyq::StlAllocator:
 	pointer allocate(
 		size_type const i_num)
 	{
+		return this->allocate(i_num, sizeof(void*));
+	}
+
+	/** @brief instance用memoryを確保する。
+	    @param[in] i_num       確保するinstanceの数。
+		@param[in] i_alignment byte単位のmemory境界値。
+	 */
+	pointer allocate(
+		size_type const   i_num,
+		std::size_t const i_alignment)
+	{
 		return static_cast< pointer >(
 			this->allocator.allocate(
-				i_num * sizeof(t_value_type), t_alignment));
+				i_num * sizeof(t_value_type), i_alignment));
 	}
 
 	//-------------------------------------------------------------------------
@@ -116,9 +124,9 @@ class psyq::StlAllocator:
 	}
 
 	//-------------------------------------------------------------------------
-	template< typename t_other_type, std::size_t t_other_alinment >
+	template< typename t_other_type >
 	bool operator==(
-		psyq::StlAllocator< t_other_type, t_other_alinment > const& i_right)
+		psyq::StlAllocator< t_other_type > const& i_right)
 		const
 	{
 		psyq::Allocator const& a_left(this->get_allocator());
@@ -126,9 +134,9 @@ class psyq::StlAllocator:
 		return &a_left == &a_right || a_left == a_right;
 	}
 
-	template< typename t_other_type, std::size_t t_other_alinment >
+	template< typename t_other_type >
 	bool operator!=(
-		psyq::StlAllocator< t_other_type, t_other_alinment > const& i_right)
+		psyq::StlAllocator< t_other_type > const& i_right)
 		const
 	{
 		return !this->operator==(i_right);

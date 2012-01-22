5e14fac8ca156ab5cfb1e7c28eb8ed32be74eb9b
psychi <psychicphysical+github.com@googlemail.com>
2012-01-22 13:59:17 +0900

    psyq::MemoryAllocator::allocateで、sizeとalignmentが0の場合の処理を実装。

--- MemoryAllocator.hpp
+++ MemoryAllocator.hpp
@@ -26,7 +26,14 @@ class psyq::DefaultMemoryInterface
 		std::size_t const i_alignment = sizeof(void*))
 	{
 		// memory境界値が2のべき乗か確認。
+		PSYQ_ASSERT(0 < i_alignment);
 		PSYQ_ASSERT(0 == (i_alignment & (i_alignment - 1)));
+
+		// sizeが0ならmemory確保しない。
+		if (i_size <= 0)
+		{
+			return nullptr;
+		}
 
 		#ifdef _WIN32
 			// win32環境でのmemory確保。
@@ -34,9 +41,12 @@ class psyq::DefaultMemoryInterface
 		#elif 200112L <= _POSIX_C_SOURCE || 600 <= _XOPEN_SOURCE
 			// posix環境でのmemory確保。
 			void* a_memory(nullptr);
-			std::size_t const a_alignment(
-				sizeof(void*) <= i_alignment? i_alignment: sizeof(void*));
-			if (0 == posix_memalign(&a_memory, a_alignment, i_size))
+			auto const a_result(
+				posix_memalign(
+					&a_memory,
+					sizeof(void*) <= i_alignment? i_alignment: sizeof(void*),
+					i_size));
+			if (0 == a_result)
 			{
 				return a_memory;
 			}
@@ -44,6 +54,7 @@ class psyq::DefaultMemoryInterface
 		#else
 			// その他の環境でのmemory確保。
 			PSYQ_ASSERT(i_alignment <= sizeof(void*));
+			(void)i_alignment;
 			return new(std::nothrow) char[i_size];
 		#endif
 	}

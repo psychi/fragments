9bb3e100fa4537041cc212eec4518c4e4be83073
psychi <psychicphysical+github.com@googlemail.com>
2012-02-07 23:08:15 +0900

    大域new/deleteをoverloadすることでmemory管理を行うようにした。

---
+++ heap_memory.hpp
@@ -0,0 +1,156 @@
+﻿#ifndef PSYQ_HEAP_MEMORY_HPP_
+#define PSYQ_HEAP_MEMORY_HPP_
+#define PSYQ_ASSERT assert
+
+namespace psyq
+{
+	class heap_memory;
+}
+
+//ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+/// heap_memory管理の基底class。
+class psyq::heap_memory
+{
+	typedef psyq::heap_memory this_type;
+
+	public:
+	//-------------------------------------------------------------------------
+	virtual ~heap_memory()
+	{
+		if (this == this_type::global_instance())
+		{
+			this_type::global_instance() = nullptr;
+		}
+	}
+
+	//-------------------------------------------------------------------------
+	/** programで最初に構築されたpsyq::heap_memoryのinstanceは、
+	    program全体で使われます。
+		これに対応するため、psyq::heap_memoryもしくは派生させたinstanceを
+	    main関数の最初でlocal変数として構築するよう実装してください。
+	    program全体で使うinstanceは、get_local関数で取得できます。
+
+	    @sa get_global
+	 */
+	heap_memory()
+	{
+		// 最初に構築されたinstanceをglobalとして使う。
+		if (nullptr == this_type::global_instance())
+		{
+			this_type::global_instance() = this;
+		}
+	}
+
+	//-------------------------------------------------------------------------
+	/** @brief heap_memoryを確保する。
+	    @param[in] i_size      確保するmemoryのbyte単位の大きさ。
+	    @param[in] i_alignment 確保するmemoryのbyte単位の境界値。
+	    @param[in] i_offset    確保するmemoryのbyte単位の境界offset値。
+	    @param[in] i_name      debugで使うためのmemory識別名。
+	 */
+	virtual void* allocate(
+		std::size_t const i_size,
+		std::size_t const i_alignment = sizeof(void*),
+		std::size_t const i_offset = 0,
+		char const* const i_name = nullptr)
+	{
+		// 未使用引数。
+		(void)i_name;
+
+		// memory境界値が2のべき乗か確認。
+		PSYQ_ASSERT(0 < i_alignment);
+		PSYQ_ASSERT(0 == (i_alignment & (i_alignment - 1)));
+
+		// sizeが0ならmemory確保しない。
+		if (i_size <= 0)
+		{
+			return nullptr;
+		}
+
+		#ifdef _WIN32
+			// win32環境でのmemory確保。
+			return _aligned_offset_malloc(i_size, i_alignment, i_offset);
+		#elif 200112L <= _POSIX_C_SOURCE || 600 <= _XOPEN_SOURCE
+			// posix環境でのmemory確保。
+			PSYQ_ASSERT(0 == i_offset);
+			void* a_memory(nullptr);
+			auto const a_result(
+				posix_memalign(
+					&a_memory,
+					sizeof(void*) <= i_alignment? i_alignment: sizeof(void*),
+					i_size));
+			if (0 == a_result)
+			{
+				return a_memory;
+			}
+		#else
+			// その他の環境でのmemory確保。
+			PSYQ_ASSERT(0 == i_offset);
+			PSYQ_ASSERT(i_alignment <= sizeof(void*));
+			(void)i_alignment;
+			return std::malloc(i_size);
+		#endif
+	}
+
+	//-------------------------------------------------------------------------
+	/** @brief memoryを解放する。
+	    @param[in] i_memory 解放するmemoryの先頭位置。
+		@param[in] i_size   解放するmemoryのbyte単位の大きさ。
+	 */
+	virtual void deallocate(
+		void* const i_memory)
+	{
+		#ifdef _WIN32
+			// win32環境でのmemory解放。
+			_aligned_free(i_memory);
+		#elif 200112L <= _POSIX_C_SOURCE || 600 <= _XOPEN_SOURCE
+			// posix環境でのmemory解放。
+			std::free(i_memory);
+		#else
+			// その他の環境でのmemory解放。
+			std::free(i_memory)
+		#endif
+	}
+
+	//-------------------------------------------------------------------------
+	bool operator==(
+		this_type const& i_right)
+		const
+	{
+		return this->identity() == i_right.identity();
+	}
+
+	bool operator!=(
+		this_type const& i_right)
+		const
+	{
+		return !this->operator==(i_right);
+	}
+
+	//-------------------------------------------------------------------------
+	/** @brief 全体で使うmemory管理instanceを取得。
+	 */
+	static this_type* get_global()
+	{
+		return this_type::global_instance();
+	}
+
+	protected:
+	//-------------------------------------------------------------------------
+	/** @brief 等価性を識別するための値を取得。
+	 */
+	virtual void const* identity() const
+	{
+		return __FUNCTION__;
+	}
+
+	private:
+	//-------------------------------------------------------------------------
+	static this_type*& global_instance()
+	{
+		static this_type* s_global(nullptr);
+		return s_global;
+	}
+};
+
+#endif // PSYQ_HEAP_MEMORY_HPP_
---
+++ new_delete.hpp
@@ -0,0 +1,120 @@
+﻿#ifndef PSYQ_NEW_DELETE_HPP_
+#define PSYQ_NEW_DELETE_HPP_
+
+// userが実装したnew/delete関数を使う場合は、
+// これより前の場所で「PSYQ_USER_NEW_DELETE」を定義すること。
+#ifndef PSYQ_USER_NEW_DELETE
+
+//-----------------------------------------------------------------------------
+/** @brief memoryを確保。
+    @param[in] i_size      確保するmemoryのbyte単位の大きさ。
+    @param[in] i_alignment byte単位のmemory境界値。2のべき乗であること。
+	@param[in] i_offset    byte単位のmemory境界offset値。
+    @param[in] i_name      debugで使うためのmemory識別名。
+ */
+void* operator new(
+	std::size_t const i_size,
+	std::size_t const i_alignment,
+	std::size_t const i_offset = 0,
+	char const* const i_name = nullptr)
+throw()
+{
+	psyq::heap_memory* const a_heap(psyq::heap_memory::get_global());
+	if (nullptr == a_heap)
+	{
+		/** @note program全体で使うpsyq::heap_memoryのinstanceがありません。
+		    psyq::heap_memoryか、そこから派生させたinstanceを、
+		    main関数の最初でlocal変数として構築するよう実装してください。
+		 */
+		PSYQ_ASSERT(false);
+		return nullptr;
+	}
+	for (;;)
+	{
+		void* const a_result(
+			a_heap->allocate(i_size, i_alignment, i_offset, i_name));
+		if (nullptr != a_result)
+		{
+			return a_result;
+		}
+
+		auto const a_handler(std::set_new_handler(nullptr));
+		if (nullptr != a_handler)
+		{
+			std::set_new_handler(a_handler);
+			a_handler();
+		}
+		else
+		{
+			return nullptr;
+		}
+	}
+}
+
+/** @brief memoryを確保。
+    @param[in] i_size 確保するmemoryのbyte単位の大きさ。
+ */
+void* operator new(
+	std::size_t const i_size)
+throw()
+{
+	return operator new(i_size, sizeof(void*));
+}
+
+/** @brief memoryを解放。
+    @param[in] i_memory 解放するmemoryの先頭位置。
+ */
+void operator delete(
+	void* const i_memory)
+throw()
+{
+	auto const a_heap(psyq::heap_memory::get_global());
+	if (nullptr != a_heap)
+	{
+		a_heap->deallocate(i_memory);
+	}
+	else
+	{
+		PSYQ_ASSERT(false);
+	}
+}
+
+//-----------------------------------------------------------------------------
+/** @brief 配列に使うmemoryを確保。
+    @param[in] i_size      確保するmemoryのbyte単位の大きさ。
+ */
+void* operator new[](
+	std::size_t const i_size)
+throw()
+{
+	return operator new(i_size, sizeof(void*));
+}
+
+/** @brief 配列に使うmemoryを確保。
+    @param[in] i_size      確保するmemoryのbyte単位の大きさ。
+    @param[in] i_alignment byte単位のmemory境界値。2のべき乗であること。
+	@param[in] i_offset    byte単位のmemory境界offset値。
+    @param[in] i_name      debugで使うためのmemory識別名。
+ */
+void* operator new[](
+	std::size_t const i_size,
+	std::size_t const i_alignment,
+	std::size_t const i_offset = 0,
+	char const* const i_name = nullptr)
+throw()
+{
+	return operator new(i_size, i_alignment, i_offset, i_name);
+}
+
+/** @brief 配列に使ったmemoryを解放。
+    @param[in] i_memory 解放するmemoryの先頭位置。
+ */
+void operator delete[](
+	void* const i_memory)
+throw()
+{
+	operator delete(i_memory);
+}
+
+#endif // PSYQ_USER_NEW_DELETE
+#endif // PSYQ_NEW_DELETE_HPP_

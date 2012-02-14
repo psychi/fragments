4162c42437903c8fb22a6bebd4f1d1de7e4ce3d4
psychi <psychicphysical+github.com@googlemail.com>
2012-02-14 19:36:53 +0900

    大域new/deleteを実装。

--- new_delete.hpp
+++ new_delete.hpp
@@ -7,6 +7,9 @@
 
 //-----------------------------------------------------------------------------
 /** @brief memoryを確保。
+    @param[in] i_size 確保するmemoryのbyte単位の大きさ。
+ */
+/** @brief memoryを確保。
     @param[in] i_size      確保するmemoryのbyte単位の大きさ。
     @param[in] i_alignment byte単位のmemory境界値。2のべき乗であること。
 	@param[in] i_offset    byte単位のmemory境界offset値。
@@ -16,44 +19,69 @@ void* operator new(
 	std::size_t const i_size,
 	std::size_t const i_alignment,
 	std::size_t const i_offset = 0,
-	char const* const i_name = nullptr)
+	char const* const i_name = NULL)
 throw()
 {
-	psyq::heap_memory* const a_heap(psyq::heap_memory::get_global());
-	if (nullptr == a_heap)
+	(void)i_name;
+
+	// memory境界値が2のべき乗か確認。
+	PSYQ_ASSERT(0 < i_alignment);
+	PSYQ_ASSERT(0 == (i_alignment & (i_alignment - 1)));
+
+	// sizeが0以下ならmemory確保しない。
+	if (i_size <= 0)
 	{
-		/** @note program全体で使うpsyq::heap_memoryのinstanceがありません。
-		    psyq::heap_memoryか、そこから派生させたinstanceを、
-		    main関数の最初でlocal変数として構築するよう実装してください。
-		 */
-		PSYQ_ASSERT(false);
-		return nullptr;
+		return NULL;
 	}
+
 	for (;;)
 	{
-		void* const a_result(
-			a_heap->allocate(i_size, i_alignment, i_offset, i_name));
-		if (nullptr != a_result)
-		{
-			return a_result;
-		}
+		#ifdef _WIN32
+			// win32環境でのmemory確保。
+			void* const a_memory(
+				_aligned_offset_malloc(i_size, i_alignment, i_offset));
+			if (NULL != a_memory)
+			{
+				return a_memory;
+			}
+		#elif 200112L <= _POSIX_C_SOURCE || 600 <= _XOPEN_SOURCE
+			// posix環境でのmemory確保。
+			PSYQ_ASSERT(0 == i_offset);
+			void* a_memory(NULL);
+			int const a_result(
+				posix_memalign(
+					&a_memory,
+					sizeof(void*) <= i_alignment? i_alignment: sizeof(void*),
+					i_size));
+			if (0 == a_result)
+			{
+				return a_memory;
+			}
+		#else
+			// その他の環境でのmemory確保。
+			PSYQ_ASSERT(0 == i_offset);
+			PSYQ_ASSERT(i_alignment <= sizeof(void*));
+			(void)i_alignment;
+			void* const a_memory(std::malloc(i_size));
+			if (NULL != a_memory)
+			{
+				return a_memory;
+			}
+		#endif
 
-		auto const a_handler(std::set_new_handler(nullptr));
-		if (nullptr != a_handler)
+		std::new_handler const a_handler(std::set_new_handler(NULL));
+		if (NULL != a_handler)
 		{
 			std::set_new_handler(a_handler);
 			a_handler();
 		}
 		else
 		{
-			return nullptr;
+			return NULL;
 		}
 	}
 }
 
-/** @brief memoryを確保。
-    @param[in] i_size 確保するmemoryのbyte単位の大きさ。
- */
 void* operator new(
 	std::size_t const i_size)
 throw()
@@ -68,28 +96,19 @@ void operator delete(
 	void* const i_memory)
 throw()
 {
-	auto const a_heap(psyq::heap_memory::get_global());
-	if (nullptr != a_heap)
-	{
-		a_heap->deallocate(i_memory);
-	}
-	else
-	{
-		PSYQ_ASSERT(false);
-	}
+	#ifdef _WIN32
+		// win32環境でのmemory解放。
+		_aligned_free(i_memory);
+	#elif 200112L <= _POSIX_C_SOURCE || 600 <= _XOPEN_SOURCE
+		// posix環境でのmemory解放。
+		std::free(i_memory);
+	#else
+		// その他の環境でのmemory解放。
+		std::free(i_memory)
+	#endif
 }
 
 //-----------------------------------------------------------------------------
-/** @brief 配列に使うmemoryを確保。
-    @param[in] i_size      確保するmemoryのbyte単位の大きさ。
- */
-void* operator new[](
-	std::size_t const i_size)
-throw()
-{
-	return operator new(i_size, sizeof(void*));
-}
-
 /** @brief 配列に使うmemoryを確保。
     @param[in] i_size      確保するmemoryのbyte単位の大きさ。
     @param[in] i_alignment byte単位のmemory境界値。2のべき乗であること。
@@ -100,10 +119,20 @@ void* operator new[](
 	std::size_t const i_size,
 	std::size_t const i_alignment,
 	std::size_t const i_offset = 0,
-	char const* const i_name = nullptr)
+	char const* const i_name = NULL)
 throw()
 {
 	return operator new(i_size, i_alignment, i_offset, i_name);
+}
+
+/** @brief 配列に使うmemoryを確保。
+    @param[in] i_size      確保するmemoryのbyte単位の大きさ。
+ */
+void* operator new[](
+	std::size_t const i_size)
+throw()
+{
+	return operator new[](i_size, sizeof(void*));
 }
 
 /** @brief 配列に使ったmemoryを解放。

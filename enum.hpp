941078ae998095cf330848b5e0725b2baa534205
psychi <psychicphysical+github.com@googlemail.com>
2012-02-23 02:44:30 +0900

    PSYQ_ENUMを実装中。

--- enum.hpp
+++ enum.hpp
@@ -5,13 +5,14 @@
 
 namespace psyq
 {
+	template< typename > class enumeration;
 	template< typename, typename > class enum_element;
 };
 
 //ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
 //-----------------------------------------------------------------------------
 /** @brief 列挙子を定義。
-    @param d_type_name  列挙子の型名。
+    @param d_enum_name  列挙子の型名。
     @param d_name_type  列挙子が持つ名前文字列の型。
     @param d_value_type 列挙子が持つ値の型。
     @param d_enum
@@ -19,54 +20,55 @@ namespace psyq
         BOOST_PP_SEQの仕様により、定義できる要素はBOOST_PP_LIMIT_REPEATが最大。
 	    boost-1.47.0では、BOOST_PP_LIMIT_REPEATは256となっている。
  */
-#define PSYQ_ENUM(d_type_name, d_name_type, d_value_type, d_elements)\
-	class d_type_name\
+#define PSYQ_ENUM(d_enum_name, d_name_type, d_value_type, d_elements)\
+	class d_enum_name:\
+		private boost::noncopyable\
 	{\
 		public:\
-		PSYQ_ENUM_ATTRIBUTE_DEFINE(\
-			d_type_name, d_name_type, d_value_type, d_elements);\
-		class element\
+		enum\
 		{\
-			public:\
 			BOOST_PP_REPEAT(\
 				BOOST_PP_SEQ_SIZE(d_elements),\
 				PSYQ_ENUM_ELEMENT_DEFINE,\
-				d_elements);\
-		};\
-		private:\
-		d_type_name();\
-	};\
-
-
-//-----------------------------------------------------------------------------
-/** @brief PSYQ_ENUMで使われるmacro。userは使用禁止。
- */
-#define PSYQ_ENUM_ATTRIBUTE_DEFINE(d_type_name, d_name_type, d_value_type, d_elements)\
-	public:\
-	typedef d_name_type name_type;\
-	typedef d_value_type value_type;\
-	typedef psyq::enum_element< d_name_type, d_value_type > element_type;\
-	typedef element_type::ordinal_type ordinal_type;\
-	enum {ordinal_max = BOOST_PP_SEQ_SIZE(d_elements)};\
-	static element_type const* find(name_type const& i_name)\
-	{\
-		for (ordinal_type i = 0; i < ordinal_max; ++i)\
-		{\
-			element_type const* const a_element(find(i));\
-			if (i_name == a_element->name) return a_element;\
-		}\
-		return NULL;\
-	}\
-	static element_type const* find(ordinal_type const i_ordinal)\
-	{\
-		static element_type const s_elements[ordinal_max] = {\
-			BOOST_PP_REPEAT(\
-				BOOST_PP_SEQ_SIZE(d_elements),\
-				PSYQ_ENUM_ELEMENT_CONSTRUCT,\
 				d_elements)\
 		};\
-		return i_ordinal < ordinal_max? &s_elements[i_ordinal]: NULL;\
-	}
+		private:\
+		d_enum_name();\
+	};\
+	template<>\
+	class psyq::enumeration< d_enum_name >:\
+		private boost::noncopyable\
+	{\
+		public:\
+		typedef d_enum_name enum_type;\
+		typedef d_name_type name_type;\
+		typedef d_value_type value_type;\
+		typedef psyq::enum_element< d_name_type, d_value_type > element_type;\
+		typedef element_type::ordinal_type ordinal_type;\
+		enum {ordinal_max = BOOST_PP_SEQ_SIZE(d_elements)};\
+		static element_type const* get(name_type const& i_name)\
+		{\
+			for (ordinal_type i = 0; i < ordinal_max; ++i)\
+			{\
+				element_type const* const a_element(get(i));\
+				if (i_name == a_element->name) return a_element;\
+			}\
+			return NULL;\
+		}\
+		static element_type const* get(ordinal_type const i_ordinal)\
+		{\
+			static element_type const s_elements[ordinal_max] = {\
+				BOOST_PP_REPEAT(\
+					BOOST_PP_SEQ_SIZE(d_elements),\
+					PSYQ_ENUM_ELEMENT_CONSTRUCT,\
+					d_elements)\
+			};\
+			return i_ordinal < ordinal_max? &s_elements[i_ordinal]: NULL;\
+		}\
+		private:\
+		enumeration();\
+	};
+
 
 //-----------------------------------------------------------------------------
 /** @brief PSYQ_ENUMで使われるmacro。userは使用禁止。
@@ -91,17 +93,7 @@ namespace psyq
 /** @brief PSYQ_ENUMで使われるmacro。userは使用禁止。
  */
 #define PSYQ_ENUM_ELEMENT_DEFINE(d_z, d_ordinal, d_elements)\
-	class BOOST_PP_SEQ_ELEM(0, BOOST_PP_SEQ_ELEM(d_ordinal, d_elements))\
-	{\
-		public:\
-		enum {ordinal = d_ordinal};\
-		static element_type const& get()\
-		{\
-			return *find(d_ordinal);\
-		}\
-		private:\
-		BOOST_PP_SEQ_ELEM(0, BOOST_PP_SEQ_ELEM(d_ordinal, d_elements))();\
-	};
+	BOOST_PP_SEQ_ELEM(0, BOOST_PP_SEQ_ELEM(d_ordinal, d_elements)),
 
 //ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
 template< typename t_name_type, typename t_value_type >

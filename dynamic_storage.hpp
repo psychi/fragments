a1a6ff3694f5d931000f2973bb53ebcdc2b588fe
psychi <psychicphysical+github.com@googlemail.com>
2012-03-30 19:03:48 +0900

    psyq::dynamic_storageを実装。

--- dynamic_storage.hpp
+++ dynamic_storage.hpp
@@ -1,16 +1,25 @@
 #ifndef PSYQ_DYNAMIC_STORAGE_HPP_
 #define PSYQ_DYNAMIC_STORAGE_HPP_
 
+namespace psyq
+{
+	class dynamic_storage;
+}
+
 //ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
-class dynamic_storage:
+/** @brief 動的に確保したmemoryを管理する。
+ */
+class psyq::dynamic_storage:
 	private boost::noncopyable
 {
-	typedef dynamic_storage this_type;
+	typedef psyq::dynamic_storage this_type;
 
 //.............................................................................
 public:
+	//-------------------------------------------------------------------------
 	~dynamic_storage()
 	{
+		// 保持しているmemoryを解放。
 		if (NULL != this->deallocator_)
 		{
 			(*this->deallocator_)(this->begin(), this->size());
@@ -19,32 +28,56 @@ public:
 
 	dynamic_storage():
 	deallocator_(NULL),
-	buffer_(NULL),
+	begin_(NULL),
 	size_(0)
 	{
 		// pass
 	}
 
 	//-------------------------------------------------------------------------
+	/** @brief memoryを確保して、保持する。
+	    @param[in] i_allocator memoryの確保に使う割当子。
+	    @param[in] i_size      確保するmemoryの大きさ。byte単位。
+	    @return 確保したmemoryの先頭位置。ただしNULLの場合は失敗。
+	 */
+	template< typename t_allocator >
+	void* allocate(
+		t_allocator const& i_allocator,
+		std::size_t const  i_size)
+	{
+		return this->allocate< typename t_allocator::allocator_policy >(
+			i_size,
+			t_allocator::alignment,
+			t_allocator::offset,
+			i_allocator.get_name());
+	}
+
+	/** @brief memoryを確保して、保持する。
+	    @param[in] i_size      確保するmemoryの大きさ。byte単位。
+	    @param[in] i_alignment 確保するmemoryの配置境界値。
+	    @param[in] i_offset    確保するmemoryの配置offset値。
+	    @param[in] i_name      debugで使うためのmemory識別名。
+	    @return 確保したmemoryの先頭位置。ただしNULLの場合は失敗。
+	 */
 	template< typename t_allocator_policy >
 	void* allocate(
 		std::size_t       i_size,
-		std::size_t       i_alignment = sizeof(void*),
-		std::size_t       i_offset = 0,
-		char const* const i_name = PSYQ_ALLOCATOR_NAME_DEFAULT)
+		std::size_t       i_alignment,
+		std::size_t       i_offset,
+		char const* const i_name)
 	{
 		if (0 < i_size)
 		{
-			void* a_buffer(
+			void* a_begin(
 				t_allocator_policy::allocate(
 					i_size, i_alignment, i_offset, i_name));
-			if (NULL != a_buffer)
+			if (NULL != a_begin)
 			{
 				this->~this_type();
 				this->deallocator_ = &t_allocator_policy::deallocate;
-				this->buffer_ = a_buffer;
+				this->begin_ = a_begin;
 				this->size_ = i_size;
-				return a_buffer;
+				return a_begin;
 			}
 		}
 		else
@@ -54,49 +87,71 @@ public:
 		return NULL;
 	}
 
+	/** @brief 保持しているmemoryを解放。
+	 */
 	void deallocate()
 	{
-		if (NULL != this->deallocator_)
-		{
-			this->~this_type();
-			this->deallocator_ = NULL;
-			this->buffer_ = NULL;
-			this->size_ = 0;
-		}
+		this_type().swap(*this);
 	}
 
 	//-------------------------------------------------------------------------
+	/** @brief 保持しているmemoryの大きさをbyte単位で取得。
+	    @return 保持しているmemoryの大きさ。byte単位。
+	 */
 	std::size_t size() const
 	{
 		return this->size_;
 	}
 
+	/** @brief 保持しているmemoryの先頭位置を取得。
+	    @return 保持しているmemoryの先頭位置。
+	 */
 	void const* begin() const
 	{
-		return this->buffer_;
+		return this->begin_;
 	}
 
+	/** @brief 保持しているmemoryの先頭位置を取得。
+	    @return 保持しているmemoryの先頭位置。
+	 */
 	void* begin()
 	{
 		return const_cast< void* >(
 			const_cast< this_type const* >(this)->begin());
 	}
 
+	/** @brief 保持しているmemoryの末尾位置を取得。
+	    @return 保持しているmemoryの末尾位置。
+	 */
 	void const* end() const
 	{
 		return static_cast< char const* >(this->begin()) + this->size();
 	}
 
+	/** @brief 保持しているmemoryの末尾位置を取得。
+	    @return 保持しているmemoryの末尾位置。
+	 */
 	void* end()
 	{
 		return const_cast< void* >(
 			const_cast< this_type const* >(this)->end());
 	}
 
+	/** @brief 保持しているmemoryを交換。
+	    @param[in,out] io_target 交換する対象。
+	 */
+	void swap(
+		this_type& io_target)
+	{
+		std::swap(this->deallocator_, io_target.deallocator_);
+		std::swap(this->begin_, io_target.begin_);
+		std::swap(this->size_, io_target.size_);
+	}
+
 //.............................................................................
 private:
 	void        (*deallocator_)(void* const, std::size_t const);
-	void*       buffer_;
+	void*       begin_;
 	std::size_t size_;
 };
 
@@ -119,15 +174,15 @@ public:
 
 	~std_file()
 	{
-		if (NULL != this->handle)
+		if (NULL != this->handle_)
 		{
-			std::fclose(this->handle);
+			std::fclose(this->handle_);
 		}
 	}
 
 	explicit std_file(
 		std::FILE* const i_handle):
-	handle(i_handle)
+	handle_(i_handle)
 	{
 		// pass
 	}
@@ -137,21 +192,21 @@ public:
 		std::size_t const i_size,
 		std::size_t const i_offset)
 	{
-		if (NULL == o_buffer || NULL == this->handle)
+		if (NULL == o_buffer || NULL == this->handle_)
 		{
 			return false;
 		}
 
-		std::fseek(this->handle, i_offset, SEEK_SET);
+		std::fseek(this->handle_, i_offset, SEEK_SET);
 		std::size_t const a_count(1);
-		return a_count == std::fread(o_buffer, i_size, a_count, this->handle);
+		return a_count == std::fread(o_buffer, i_size, a_count, this->handle_);
 	}
 
 	std::size_t get_size() const
 	{
-		return NULL != this->handle
-			&& 0 == std::fseek(this->handle, 0, SEEK_END)?
-				std::ftell(this->handle): 0;
+		return NULL != this->handle_
+			&& 0 == std::fseek(this->handle_, 0, SEEK_END)?
+				std::ftell(this->handle_): 0;
 	}
 
 	template< typename t_allocator >
@@ -196,7 +251,7 @@ public:
 	}
 
 private:
-	std::FILE* handle;
+	std::FILE* handle_;
 };
 
 //ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
@@ -206,8 +261,8 @@ class file_task
 	typedef file_task this_type;
 
 public:
-	typedef std::tr1::shared_ptr< this_type > holder;
-	typedef std::tr1::weak_ptr< this_type > observer;
+	typedef boost::shared_ptr< this_type > holder;
+	typedef boost::weak_ptr< this_type > observer;
 
 	enum state
 	{
@@ -228,89 +283,85 @@ class file_read_task:
 	public file_task
 {
 public:
-	typedef std::tr1::shared_ptr< this_type > holder;
-	typedef std::tr1::weak_ptr< this_type > observer;
+	typedef boost::shared_ptr< this_type > holder;
+	typedef boost::weak_ptr< this_type > observer;
 
 	enum state
 	{
-		state_READ_ERROR = -0x80000000,
+		state_READ_ERROR = 0x80000000,
 	};
 
-	virtual ~file_read_task()
+	template< typename t_allocator >
+	static this_type::holder create(
+		t_allocator const&      i_allocator,
+		std_file::holder const& i_file,
+		std::size_t const       i_read_size = i_file->get_size(),
+		std::size_t const       i_read_offset = 0)
 	{
- 		std::size_t const a_unit_size(sizeof(typename t_allocator::value_type));
-		this->allocator.deallocate(
-			this->buffer, (this->size + a_unit_size - 1) / a_unit_size);
+		return this_type::create(
+			i_allocator, i_allocator, i_file, i_read_size, i_read_offset);
 	}
 
-	template< typename t_other_allocator >
+	template< typename t_holder_allocator, typename t_storage_allocator >
 	static this_type::holder create(
-		t_other_allocator&      io_allocator,
-		std_file::holder const& i_file,
-		std::size_t const       i_read_size,
-		std::size_t const       i_read_offset,
-		t_allocator const&      i_buffer_allocator = io_allocator)
+		t_holder_allocator const&  i_holder_allocator,
+		t_storage_allocator const& i_storage_allocator,
+		std_file::holder const&    i_file,
+		std::size_t const          i_read_size = i_file->get_size(),
+		std::size_t const          i_read_offset = 0)
 	{
 		std_file* const a_file(i_file.get());
 		if (NULL != a_file)
 		{
-			return boost::allocate_shared(
-				io_allocator,
-				i_file,
-				i_read_size,
-				i_read_offset,
-				i_buffer_allocator);
+			// 読み込みbufferの大きさを決定。
+			std::size_t a_size(a_file->get_size());
+			if (i_read_size < a_size)
+			{
+				a_size = i_read_size;
+			}
+
+			// taskを生成。
+			this_type::holder const a_task(
+				boost::allocate_shared< this_type >(
+					i_holder_allocator, i_file, a_size));
+			if (NULL != a_task.get())
+			{
+				// 読み込みbufferを確保。
+				psyq::dynamic_storage& a_storage(a_task->storage_);
+				if (NULL != a_storage.allocate(i_storage_allocator, a_size))
+				{
+					return a_task;
+				}
+			}
 		}
 		return this_type::holder();
 	}
 
-	template< typename t_other_allocator >
-	static this_type::holder create(
-		t_other_allocator&      io_allocator,
-		std_file::holder const& i_file,
-		t_allocator const&      i_buffer_allocator = io_allocator)
-	{
-		std_file* const a_file(i_file.get());
-		if (NULL != a_file)
-		{
-			return this_type::create(
-				io_allocator,
-				i_file,
-				a_file->get_size(),
-				0,
-				i_buffer_allocator);
-		}
-		return this_type::holder();
-	}
-
+protected:
 	file_read_task(
 		std_file::holder const& i_file,
-		std::size_t const       i_size,
-		std::size_t const       i_offset,
-		t_allocator const&      i_allocator):
-	file(i_file),
-	size(i_size),
-	offset(i_offset),
-	allocator(i_allocator)
+		std::size_t const       i_offset):
+	file_(i_file),
+	offset_(i_offset)
 	{
-		std::size_t const a_unit_size(sizeof(typename t_allocator::value_type));
-		this->buffer = this->allocator.allocate(
-			(i_size + a_unit_size - 1) / a_unit_size);
-		PSYQ_ASSERT(NULL != this->buffer);
+		// pass
 	}
 
 	virtual int execute()
 	{
-		return this->file->read(this->buffer, this->size, this->offset)?
+		int const a_result(
+			this->file->read(
+				this->storage_.begin(),
+				this->storage_.size(),
+				this->offset_));
+		return a_result?
 			this_type::state_SUCCESS: this_type::state_READ_ERROR;
 	}
 
 private:
-	std_file::holder file;
-	std::size_t      size;
-	std::size_t      offset;
-	t_allocator      allocator;
-	void*            buffer;
+	psyq::dynamic_storage storage_;
+	std_file::holder      file_;
+	std::size_t           offset_;
 };
 
 //ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ

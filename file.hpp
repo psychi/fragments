6ee7702c4dba4568580e2a00e0dc1db10234acc4
psychi <psychicphysical+github.com@googlemail.com>
2012-04-10 12:29:29 +0900

    非同期file処理を実装中。

--- file.hpp
+++ file.hpp
@@ -2,6 +2,7 @@
 #define PSYQ_FILE_HPP_
 
 #include <boost/bind/bind.hpp>
+#include <boost/thread/thread.hpp>
 #include <boost/thread/condition.hpp>
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/once.hpp>
@@ -18,20 +19,41 @@ public:
 	typedef boost::shared_ptr< this_type > holder;
 	typedef boost::weak_ptr< this_type > observer;
 
+	enum state
+	{
+		state_IDLE,
+		state_BUSY,
+		state_ABORTED,
+		state_FINISHED,
+		state_end
+	};
+
+	virtual ~file_task()
+	{
+		PSYQ_ASSERT(this_type::state_BUSY != this->get_state());
+	}
+
+	boost::int32_t get_state() const
+	{
+		return this->state_;
+	}
+
 //.............................................................................
 protected:
 	file_task():
-	next_(NULL)
+	next_(NULL),
+	state_(this_type::state_IDLE)
 	{
 		// pass
 	}
 
-	virtual void run() = 0;
+	virtual boost::int32_t run() = 0;
 
 //.............................................................................
 private:
 	this_type::holder lock_;
 	this_type*        next_;
+	boost::int32_t    state_;
 };
 
 //ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
@@ -45,6 +67,7 @@ public:
 	~file_server()
 	{
 		this->stop(true);
+		PSYQ_ASSERT(NULL == this->queue_);
 	}
 
 	file_server():
@@ -57,51 +80,33 @@ public:
 	}
 
 	//-------------------------------------------------------------------------
-	void start()
-	{
-		if (!this->thread_.joinable())
-		{
-			boost::thread(boost::bind(&this_type::run, this)).swap(this->thread_);
-		}
-	}
-
-	void stop(bool const i_sync = true)
-	{
-		this->stop_ = true;
-		if (i_sync)
-		{
-			this->condition_.notify_all();
-			this->thread_.join();
-		}
-	}
-
-	//-------------------------------------------------------------------------
 	/** @brief file-taskを追加。
 	    @param[in] i_task 追加するfile-task。
 	 */
 	void add(file_task::holder const& i_task)
 	{
-		file_task* const a_task(i_task.get());
-		if (NULL != a_task && NULL == a_task->lock_.get())
+		this->add(&i_task, &i_task + 1);
+	}
+
+	/** @brief file-taskを追加。
+	    @param[in] i_begin 追加するfile-task-containerの先頭位置。
+	    @param[in] i_end   追加するfile-task-containerの末尾位置。
+	 */
+	template< typename t_iterator >
+	void add(t_iterator const i_begin, t_iterator const i_end)
+	{
+		if (i_begin != i_end)
 		{
 			boost::lock_guard< boost::mutex > const a_lock(
 				this_type::class_mutex());
-
-			// file-taskをlockする。
-			a_task->lock_ = i_task;
-
-			// file-taskを待ち行列の末尾に挿入。
-			file_task* const a_back(this->queue_);
-			this->queue_ = a_task;
-			if (NULL != a_back)
+			bool const a_notify(NULL == this->queue_);
+			for (t_iterator i = i_begin; i_end != i; ++i)
 			{
-				a_task->next_ = a_back->next_;
-				a_back->next_ = a_task;
+				this->queue_ = this_type::push_back(this->queue_, *i);
 			}
-			else
+			if (a_notify)
 			{
 				// 待ち行列が空だったので、threadを起動。
-				a_task->next_ = a_task;
 				this->condition_.notify_all();
 			}
 		}
@@ -110,19 +115,56 @@ public:
 //.............................................................................
 private:
 	//-------------------------------------------------------------------------
-	void run()
+	void start()
 	{
-		while (!this->stop_)
+		if (!this->thread_.joinable())
 		{
-			file_task::holder const a_task(this->pop_front());
-			if (!a_task.unique() && NULL != a_task.get())
-			{
-				a_task->run();
-			}
+			boost::thread(boost::bind(&this_type::run, this)).swap(
+				this->thread_);
+		}
+	}
+
+	void stop(bool const i_sync = true)
+	{
+		// 終了flagを有効化。
+		this->stop_ = true;
+		if (i_sync)
+		{
+			// threadの終了まで待つ。
+			this->condition_.notify_all();
+			this->thread_.join();
 		}
 	}
 
 	//-------------------------------------------------------------------------
+	/** @brief 待ち行列の末尾に挿入。
+	 */
+	static file_task* push_back(
+		file_task* const         i_back,
+		file_task::holder const& i_task)
+	{
+		// file-taskをlockする。
+		file_task* const a_task(i_task.get());
+		if (NULL == a_task)
+		{
+			return i_back;
+		}
+		a_task->lock_ = i_task;
+		a_task->state_ = file_task::state_BUSY;
+
+		// file-taskを待ち行列の末尾に挿入。
+		if (NULL != i_back)
+		{
+			a_task->next_ = i_back->next_;
+			i_back->next_ = a_task;
+		}
+		else
+		{
+			a_task->next_ = a_task;
+		}
+		return a_task;
+	}
+
 	/** @brief 待ち行列の先頭を取り出す。
 	 */
 	file_task::holder pop_front()
@@ -154,6 +196,25 @@ private:
 		file_task::holder a_holder;
 		a_holder.swap(a_task->lock_);
 		return a_holder;
+	}
+
+	//-------------------------------------------------------------------------
+	void run()
+	{
+		for (;;)
+		{
+			// 待ち行列の先頭を取り出し、実行する。
+			file_task::holder const a_task(this->pop_front());
+			if (NULL != a_task.get())
+			{
+				a_task->state_ = a_task.unique()?
+					file_task::state_ABORTED: a_task->run();
+			}
+			else if (this->stop_)
+			{
+				break;
+			}
+		}
 	}
 
 	//-------------------------------------------------------------------------

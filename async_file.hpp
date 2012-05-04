1fa6b417fa8d436102c55c8ee4b1b279c2ea453d
psychi <psychicphysical+github.com@googlemail.com>
2012-05-04 18:01:10 +0900

    psyq::async_file_readerとpsyq::async_file_writerの共通部分を、psyq::_async_file_taskにまとめた。

--- async_file.hpp
+++ async_file.hpp
@@ -7,6 +7,7 @@
 namespace psyq
 {
 	class file_buffer;
+	template< typename > class _async_file_task;
 	template< typename, typename > class async_file_reader;
 	template< typename > class async_file_writer;
 	class async_file_mapper;
@@ -163,19 +164,79 @@ private:
 };
 
 //ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
-template< typename t_file, typename t_arena = psyq::heap_arena >
-class psyq::async_file_reader:
+template< typename t_file >
+class psyq::_async_file_task:
 	public psyq::async_task
 {
-	typedef psyq::async_file_reader< t_file, t_arena > this_type;
+	typedef psyq::_async_file_task< t_file > this_type;
 	typedef psyq::async_task super_type;
 
 //.............................................................................
 public:
+	typedef t_file file;
 	typedef boost::shared_ptr< this_type > shared_ptr;
 	typedef boost::weak_ptr< this_type > weak_ptr;
 
-	typedef t_file file;
+	//-------------------------------------------------------------------------
+	/** @brief fileを取得。
+	 */
+	typename t_file::shared_ptr const& get_file() const
+	{
+		return this->file_;
+	}
+
+	/** @brief file-bufferを取得。
+	 */
+	psyq::file_buffer const* get_buffer() const
+	{
+		return super_type::state_BUSY != this->get_state()?
+			&this->buffer_: NULL;
+	}
+
+	/** @brief file-bufferを取得。
+	 */
+	psyq::file_buffer* get_buffer()
+	{
+		return const_cast< psyq::file_buffer* >(
+			const_cast< this_type const* >(this)->get_buffer());
+	}
+
+	/** @brief error番号を取得。
+	    @return error番号。errorがない場合は0。
+	 */
+	int get_error() const
+	{
+		return super_type::state_BUSY != this->get_state()?
+			this->error_: 0;
+	}
+
+//.............................................................................
+protected:
+	explicit _async_file_task(typename t_file::shared_ptr const& i_file):
+	file_(i_file),
+	error_(0)
+	{
+		PSYQ_ASSERT(NULL != i_file.get());
+		PSYQ_ASSERT(i_file->is_open());
+	}
+
+//.............................................................................
+protected:
+	typename t_file::shared_ptr file_;
+	psyq::file_buffer           buffer_;
+	int                         error_;
+};
+
+//ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+template< typename t_file, typename t_arena = psyq::heap_arena >
+class psyq::async_file_reader:
+	public psyq::_async_file_task< t_file >
+{
+	typedef psyq::async_file_reader< t_file, t_arena > this_type;
+	typedef psyq::_async_file_task< t_file > super_type;
+
+//.............................................................................
+public:
 	typedef t_arena arena;
 
 	//-------------------------------------------------------------------------
@@ -197,40 +258,13 @@ public:
 		std::size_t const                  i_buffer_alignment = 0,
 		const char* const                  i_arena_name
 			= PSYQ_ARENA_NAME_DEFAULT):
-	file_(i_file),
+	super_type(i_file),
+	arena_name_(i_arena_name),
 	buffer_alignment_(i_buffer_alignment),
 	read_offset_(i_read_offset),
-	read_size_(i_read_size),
-	arena_name_(i_arena_name)
+	read_size_(i_read_size)
 	{
-		PSYQ_ASSERT(NULL != i_file.get());
-		PSYQ_ASSERT(i_file->is_open());
-	}
-
-	//-------------------------------------------------------------------------
-	/** @brief 読み込みbufferを取得。
-	 */
-	psyq::file_buffer const* get_buffer() const
-	{
-		return super_type::state_BUSY != this->get_state()?
-			&this->buffer_: NULL;
-	}
-
-	/** @brief 読み込みbufferを取得。
-	 */
-	psyq::file_buffer* get_buffer()
-	{
-		return const_cast< psyq::file_buffer* >(
-			const_cast< this_type const* >(this)->get_buffer());
-	}
-
-	/** @brief 読み込みerror番号を取得。
-	    @return 読み込みerror番号。errorがない場合は0。
-	 */
-	int get_error() const
-	{
-		return super_type::state_BUSY != this->get_state()?
-			this->error_: 0;
+		// pass
 	}
 
 //.............................................................................
@@ -276,63 +310,30 @@ private:
 
 //.............................................................................
 private:
-	typename t_file::shared_ptr file_;
-	psyq::file_buffer           buffer_;
-	std::size_t                 buffer_alignment_;
-	std::size_t                 read_offset_;
-	std::size_t                 read_size_;
-	int                         error_;
-	char const*                 arena_name_;
+	char const* arena_name_;
+	std::size_t buffer_alignment_;
+	std::size_t read_offset_;
+	std::size_t read_size_;
 };
 
 //ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
 template< typename t_file >
 class psyq::async_file_writer:
-	public psyq::async_task
+	public psyq::_async_file_task< t_file >
 {
 	typedef psyq::async_file_writer< t_file > this_type;
-	typedef psyq::async_task super_type;
+	typedef psyq::_async_file_task< t_file > super_type;
 
 //.............................................................................
 public:
-	typedef t_file file;
-
 	//-------------------------------------------------------------------------
 	async_file_writer(
 		typename t_file::shared_ptr const& i_file,
 		psyq::file_buffer&                 io_buffer):
-	file_(i_file),
+	super_type(i_file),
 	write_size_(0)
 	{
-		PSYQ_ASSERT(NULL != i_file.get());
-		PSYQ_ASSERT(i_file->is_open());
 		this->buffer_.swap(io_buffer);
-	}
-
-	//-------------------------------------------------------------------------
-	/** @brief 書き出しbufferを取得。
-	 */
-	psyq::file_buffer const* get_buffer() const
-	{
-		return super_type::state_BUSY != this->get_state()?
-			&this->buffer_: NULL;
-	}
-
-	/** @brief 書き出しbufferを取得。
-	 */
-	psyq::file_buffer* get_buffer()
-	{
-		return const_cast< psyq::file_buffer* >(
-			const_cast< this_type const* >(this)->get_buffer());
-	}
-
-	/** @brief 書き出しerror番号を取得。
-	    @return 書き出しerror番号。errorがない場合は0。
-	 */
-	int get_error() const
-	{
-		return super_type::state_BUSY != this->get_state()?
-			this->error_: 0;
 	}
 
 //.............................................................................
@@ -349,10 +350,7 @@ private:
 
 //.............................................................................
 private:
-	typename t_file::shared_ptr file_;
-	psyq::file_buffer           buffer_;
-	std::size_t                 write_size_;
-	int                         error_;
+	std::size_t write_size_;
 };
 
 //ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ

0e07f6425011a00a62d46b30f8b49d630b6114a7
psychi <psychicphysical+github.com@googlemail.com>
2012-05-12 03:39:58 +0900

    psyq::async_queueで使っているqueueをclass化。

--- async_queue.hpp
+++ async_queue.hpp
@@ -26,7 +26,7 @@ public:
 	~async_queue()
 	{
 		this->stop(true);
-		this_type::abort_tasks(this->reserve_storage_);
+		this->reserve_queue_.abort();
 	}
 
 	explicit async_queue():
@@ -41,13 +41,13 @@ public:
 	{
 		boost::lock_guard< boost::mutex > const a_lock(
 			const_cast< boost::mutex& >(this->mutex_));
-		return this->reserve_storage_.get_size() <= 0
-			&& this->running_size_ <= 0;
+		return this->reserve_queue_.is_empty() && this->running_size_ <= 0;
 	}
 
 	//-------------------------------------------------------------------------
 	/** @brief 非同期処理taskを登録。
 	    @param[in] i_task 登録する非同期処理taskを指すsmart-pointer。
+	    @param[in] i_name 確保するmemoryの識別名。debugでのみ使う。
 	    @return 登録した非同期処理taskの数。
 	 */
 	template< typename t_arena >
@@ -62,6 +62,7 @@ public:
 	    @tparam t_iterator shared_ptr型のcontainerのiterator型。
 	    @param[in] i_begin containerの先頭位置。
 	    @param[in] i_end   containerの末尾位置。
+	    @param[in] i_name  確保するmemoryの識別名。debugでのみ使う。
 	    @return 登録した非同期処理taskの数。
 	 */
 	template< typename t_arena, typename t_iterator >
@@ -71,20 +72,18 @@ public:
 		char const* const i_name = PSYQ_ARENA_NAME_DEFAULT)
 	{
 		// 予約queueを取り出す。
+		this_type::task_queue a_queue;
 		boost::lock_guard< boost::mutex > const a_lock(this->mutex_);
-		psyq::dynamic_storage a_storage;
-		a_storage.swap(this->reserve_storage_);
+		this->reserve_queue_.swap(a_queue);
 		std::size_t const a_last_size(
-			0 < a_storage.get_size()?
-				a_storage.get_size() / sizeof(this_type::task_ptr):
-				this->running_size_);
+			a_queue.is_empty()? this->running_size_: a_queue.get_size());
 
 		// 新しいqueueを構築。
 		std::size_t const a_new_capacity(
 			a_last_size + std::distance(i_begin, i_end));
 		this_type::task_ptr* const a_new_tasks(
-			this_type::resize_tasks< t_arena >(
-				a_storage, a_last_size, a_new_capacity, i_name));
+			a_queue.template resize< t_arena >(
+				a_last_size, a_new_capacity, i_name));
 
 		// containerが持つ非同期処理taskを、新しいqueueにcopy。
 		std::size_t a_count(0);
@@ -106,12 +105,13 @@ public:
 		}
 
 		// 新しいqueueを予約queueに差し替えてから通知。
-		this->reserve_storage_.swap(a_storage);
+		this->reserve_queue_.swap(a_queue);
 		this->condition_.notify_all();
 		return a_count;
 	}
 
 	/** @brief queueの大きさを必要最低限にする。
+	    @param[in] i_name 確保するmemoryの識別名。debugでのみ使う。
 	 */
 	template< typename t_arena >
 	void shrink(char const* const i_name = PSYQ_ARENA_NAME_DEFAULT)
@@ -125,6 +125,156 @@ public:
 //.............................................................................
 private:
 	typedef psyq::async_task::weak_ptr task_ptr;
+
+	//-------------------------------------------------------------------------
+	template< typename t_value >
+	class container:
+		private psyq::dynamic_storage
+	{
+		typedef container this_type;
+		typedef psyq::dynamic_storage super_type;
+
+	public:
+		~container()
+		{
+			t_value* const a_tasks(this->get_address());
+			std::size_t const a_size(this->get_size());
+			for (std::size_t i = 0; i < a_size; ++i)
+			{
+				a_tasks[i].~t_value();
+			}
+		}
+
+		container():
+		super_type()
+		{
+			// pass
+		}
+
+		void swap(this_type& io_target)
+		{
+			this->super_type::swap(io_target);
+		}
+
+		t_value* get_address()
+		{
+			return static_cast< t_value* >(this->super_type::get_address());
+		}
+
+		std::size_t get_size() const
+		{
+			return this->super_type::get_size() / sizeof(t_value);
+		}
+
+		bool is_empty() const
+		{
+			return this->super_type::get_size() <= 0;
+		}
+
+		/** @brief containerの大きさを変更する。
+		    @param[in] i_last_size    元のcontainerの要素数。
+		    @param[in] i_new_capacity 新しいcontainerの最大要素数。
+			@param[in] i_memory_name  確保するmemoryの識別名。debugでのみ使う。
+		    @return 新しいcontainerの先頭位置。
+		 */
+		template< typename t_arena >
+		t_value* resize(
+			std::size_t const i_last_size,
+			std::size_t const i_new_capacity,
+			char const* const i_memory_name)
+		{
+			PSYQ_ASSERT(i_last_size <= i_new_capacity);
+
+			// 新しいcontainerを確保。
+			this_type a_container(
+				boost::type< t_arena >(),
+				i_new_capacity * sizeof(t_value),
+				boost::alignment_of< t_value >::value,
+				0,
+				i_memory_name);
+			t_value* const a_new_tasks(a_container.get_address());
+			if (NULL != a_new_tasks)
+			{
+				// 元のcontainerが持つ非同期処理taskを、新しいcontainerに移動。
+				t_value* const a_last_tasks(this->get_address());
+				for (std::size_t i = 0; i < i_last_size; ++i)
+				{
+					new(&a_new_tasks[i]) typename t_value();
+					if (NULL != a_last_tasks)
+					{
+						a_new_tasks[i].swap(a_last_tasks[i]);
+					}
+				}
+			}
+			else
+			{
+				PSYQ_ASSERT(i_new_capacity <= 0);
+			}
+			this->swap(a_container);
+			return a_new_tasks;
+		}
+
+		/** @brief containerが持つ非同期処理taskを実行する。
+		    @param[in] i_size containerが持つ非同期処理taskの数。
+		    @return containerが持つ非同期処理taskの数。
+		 */
+		std::size_t run(std::size_t const i_size)
+		{
+			t_value* const a_tasks(this->get_address());
+			std::size_t a_size(0);
+			for (std::size_t i = 0; i < i_size; ++i)
+			{
+				psyq::async_task::shared_ptr const a_holder(a_tasks[i].lock());
+				psyq::async_task* const a_task(a_holder.get());
+				if (NULL != a_task
+					&& psyq::async_task::state_BUSY == a_task->get_state())
+				{
+					boost::int32_t const a_state(a_task->run());
+					if (psyq::async_task::state_BUSY == a_state)
+					{
+						a_tasks[a_size].swap(a_tasks[i]);
+						++a_size;
+						continue;
+					}
+					a_task->set_unlocked_state(a_state);
+				}
+				a_tasks[i].reset();
+			}
+			return a_size;
+		}
+
+		/** @brief containerが持つ非同期処理taskを途中終了する。
+		 */
+		void abort()
+		{
+			t_value* const a_tasks(this->get_address());
+			std::size_t const a_size(this->get_size());
+			for (std::size_t i = 0; i < a_size; ++i)
+			{
+				psyq::async_task::shared_ptr const a_holder(a_tasks[i].lock());
+				psyq::async_task* const a_task(a_holder.get());
+				if (NULL != a_task
+					&& psyq::async_task::state_BUSY == a_task->get_state())
+				{
+					a_task->set_unlocked_state(psyq::async_task::state_ABORTED);
+				}
+			}
+		}
+
+	private:
+		template< typename t_arena >
+		container(
+			boost::type< t_arena > const& i_type,
+			std::size_t const             i_size,
+			std::size_t const             i_alignment,
+			std::size_t const             i_offset,
+			char const* const             i_name):
+		super_type(i_type, i_size, i_alignment, i_offset, i_name)
+		{
+			// pass
+		}
+	};
+	typedef this_type::container< this_type::task_ptr > task_queue;
 
 	//-------------------------------------------------------------------------
 	void start()
@@ -151,20 +301,20 @@ private:
 
 	void run()
 	{
-		psyq::dynamic_storage a_storage;
+		this_type::task_queue a_queue;
 		std::size_t a_size(0);
 		boost::unique_lock< boost::mutex > a_lock(this->mutex_);
 		while (!this->stop_)
 		{
 			// 実行queueを更新。
-			if (0 < this->reserve_storage_.get_size())
+			if (!this->reserve_queue_.is_empty())
 			{
 				// 予約queueを、新しい実行queueとして取り出す。
-				psyq::dynamic_storage a_last_storage;
-				a_last_storage.swap(a_storage);
-				this->reserve_storage_.swap(a_storage);
+				this_type::task_queue a_last_queue;
+				a_last_queue.swap(a_queue);
+				this->reserve_queue_.swap(a_queue);
 				std::size_t const a_last_size(a_size);
-				a_size = a_storage.get_size() / sizeof(this_type::task_ptr);
+				a_size = a_queue.get_size();
 				PSYQ_ASSERT(this->running_size_ <= a_size);
 				PSYQ_ASSERT(a_last_size <= a_size);
 				this->running_size_
@@ -174,18 +324,15 @@ private:
 				// 元の実行queueが持つtaskを、新しい実行queueに移動。
 				this_type::task_ptr* const a_new_tasks(
 					static_cast< this_type::task_ptr* >(
-						a_storage.get_address()));
+						a_queue.get_address()));
 				this_type::task_ptr* const a_last_tasks(
 					static_cast< this_type::task_ptr* >(
-						a_last_storage.get_address()));
+						a_last_queue.get_address()));
 				for (std::size_t i = 0; i < a_last_size; ++i)
 				{
 					PSYQ_ASSERT(a_new_tasks[i].expired());
 					a_new_tasks[i].swap(a_last_tasks[i]);
 				}
-
-				// 元の実行queueを破棄。
-				this_type::destroy_tasks(a_last_storage);
 			}
 			else if (0 < a_size)
 			{
@@ -201,137 +348,15 @@ private:
 			}
 
 			// 実行queueのtaskを呼び出す。
-			a_size = this_type::run_tasks(
-				static_cast< this_type::task_ptr* >(a_storage.get_address()),
-				a_size);
+			a_size = a_queue.run(a_size);
 			if (a_size <= 0)
 			{
 				// 実行queueが空になったので破棄。
-				this_type::destroy_tasks(a_storage);
+				this_type::task_queue().swap(a_queue);
 			}
 			a_lock.lock();
 		}
-		this_type::abort_tasks(a_storage);
-	}
-
-	//-------------------------------------------------------------------------
-	/** @brief queueが持つ非同期処理taskを実行する。
-	    @param[in] io_tasks queueの先頭位置。
-	    @param[in] i_size   queueが持つ非同期処理taskの数。
-	    @return queueが持つ非同期処理taskの数。
-	 */
-	static std::size_t run_tasks(
-		this_type::task_ptr* const io_tasks,
-		std::size_t const          i_size)
-	{
-		std::size_t a_size(0);
-		for (std::size_t i = 0; i < i_size; ++i)
-		{
-			psyq::async_task::shared_ptr const a_holder(io_tasks[i].lock());
-			psyq::async_task* const a_task(a_holder.get());
-			if (NULL != a_task
-				&& psyq::async_task::state_BUSY == a_task->get_state())
-			{
-				boost::int32_t const a_state(a_task->run());
-				if (psyq::async_task::state_BUSY == a_state)
-				{
-					io_tasks[a_size].swap(io_tasks[i]);
-					++a_size;
-					continue;
-				}
-				a_task->set_unlocked_state(a_state);
-			}
-			io_tasks[i].reset();
-		}
-		return a_size;
-	}
-
-	/** @brief queueの大きさを変更する。
-	    @param[in,out] io_storage queueとして使っている領域。
-	    @param[in] i_last_size    元のqueueの要素数。
-	    @param[in] i_new_capacity 新しいqueueの最大要素数。
-		@return 新しいqueueの先頭位置。
-	 */
-	template< typename t_arena >
-	static this_type::task_ptr* resize_tasks(
-		psyq::dynamic_storage& io_storage,
-		std::size_t const      i_last_size,
-		std::size_t const      i_new_capacity,
-		char const* const      i_memory_name)
-	{
-		// 新しいstorageを確保。
-		psyq::dynamic_storage a_storage(
-			boost::type< t_arena >(),
-			i_new_capacity * sizeof(this_type::task_ptr),
-			boost::alignment_of< this_type::task_ptr >::value,
-			0,
-			i_memory_name);
-		this_type::task_ptr* const a_new_tasks(
-			static_cast< this_type::task_ptr* >(a_storage.get_address()));
-		if (NULL != a_new_tasks)
-		{
-			PSYQ_ASSERT(i_last_size <= i_new_capacity);
-
-			// 元のqueueが持つ非同期処理taskを、新しいqueueに移動。
-			this_type::task_ptr* const a_last_tasks(
-				static_cast< this_type::task_ptr* >(io_storage.get_address()));
-			for (std::size_t i = 0; i < i_last_size; ++i)
-			{
-				new(&a_new_tasks[i]) typename this_type::task_ptr();
-				if (NULL != a_last_tasks)
-				{
-					a_new_tasks[i].swap(a_last_tasks[i]);
-				}
-			}
-		}
-		else
-		{
-			PSYQ_ASSERT(i_new_capacity <= 0);
-		}
-		io_storage.swap(a_storage);
-		this_type::destroy_tasks(a_storage);
-		return a_new_tasks;
-	}
-
-	/** @brief 非同期処理task行列を途中終了する。
-	    @param[in,out] io_storage queueとして使っている領域。
-	 */
-	static void abort_tasks(psyq::dynamic_storage& io_storage)
-	{
-		this_type::task_ptr* const a_tasks(
-			static_cast< this_type::task_ptr* >(io_storage.get_address()));
-		std::size_t const a_capacity(
-			io_storage.get_size() / sizeof(this_type::task_ptr));
-		for (std::size_t i = 0; i < a_capacity; ++i)
-		{
-			psyq::async_task::shared_ptr const a_holder(a_tasks[i].lock());
-			psyq::async_task* const a_task(a_holder.get());
-			if (NULL != a_task
-				&& psyq::async_task::state_BUSY == a_task->get_state())
-			{
-				a_task->set_unlocked_state(
-					psyq::async_task::state_ABORTED);
-			}
-		}
-		this_type::destroy_tasks(io_storage);
-	}
-
-	/** @brief 非同期処理task行列を破棄する。
-	    @param[in,out] io_storage queueとして使っている領域。
-	 */
-	static void destroy_tasks(psyq::dynamic_storage& io_storage)
-	{
-		PSYQ_ASSERT(
-			NULL != io_storage.get_address() || io_storage.get_size() <= 0);
-		this_type::task_ptr* const a_tasks(
-			static_cast< this_type::task_ptr* >(io_storage.get_address()));
-		std::size_t const a_capacity(
-			io_storage.get_size() / sizeof(this_type::task_ptr));
-		for (std::size_t i = 0; i < a_capacity; ++i)
-		{
-			a_tasks[i].~task_ptr();
-		}
-		psyq::dynamic_storage().swap(io_storage);
+		a_queue.abort();
 	}
 
 //.............................................................................
@@ -339,8 +364,8 @@ private:
 	boost::thread         thread_;
 	boost::condition      condition_;
 	boost::mutex          mutex_;
-	psyq::dynamic_storage reserve_storage_;
-	std::size_t           running_size_; ///< 実行queueの要素数。
+	this_type::task_queue reserve_queue_; ///< 予約task-queue。
+	std::size_t           running_size_;  ///< 実行task-queueの要素数。
 	bool                  stop_;
 };
 

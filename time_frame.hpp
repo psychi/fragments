e2efaeecd9e2fa637abbd0de78ec99c13e93c9b8
psychi <psychicphysical+github.com@googlemail.com>
2012-06-13 20:59:50 +0900

    psyq::time_frameを整理。

--- time_frame.hpp
+++ time_frame.hpp
@@ -5,48 +5,51 @@
 
 namespace psyq
 {
-	template< typename, typename > class time_frame;
+	template< typename, typename, typename > class time_frame;
 }
 
 //ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
-template< typename t_scale, typename t_count >
+template< typename t_scale, typename t_count, typename t_tag = void >
 class psyq::time_frame
 {
-	typedef psyq::time_frame< t_scale, t_count > this_type;
+	typedef psyq::time_frame< t_scale, t_count, t_tag > this_type;
 
 //.............................................................................
 public:
+	typedef t_scale scale;
+	typedef t_count count;
+	typedef t_tag tag;
+	typedef psyq::lerp< t_scale, t_count > lerp;
 	typedef PSYQ_SHARED_PTR< this_type > shared_ptr;
 	typedef PSYQ_WEAK_PTR< this_type > weak_ptr;
 
-	typedef t_count count;
-	typedef psyq::lerp< t_scale, t_count > scale;
-
 	//-------------------------------------------------------------------------
 	explicit time_frame(
-		t_scale const                         i_scale = 1,
+		typename this_type::lerp const&       i_scale = this_type::lerp(1),
 		typename this_type::shared_ptr const& i_super =
 			this_type::shared_ptr()):
 	super_(i_super),
 	scale_(i_scale),
-	current_(i_scale)
+	current_(i_scale.current()),
+	count_(this_type::frame_count())
 	{
-		this->construct(i_super.get());
-	}
-
-	explicit time_frame(
-		typename this_type::scale const&      i_scale,
-		typename this_type::shared_ptr const& i_super =
-			this_type::shared_ptr()):
-	super_(i_super),
-	scale_(i_scale),
-	current_(i_scale.current())
-	{
-		this->construct(i_super.get());
+		this_type* const a_super(i_super.get());
+		if (NULL != a_super)
+		{
+			if (a_super->find_super(*this))
+			{
+				PSYQ_ASSERT(false);
+				this->super_.reset();
+			}
+			else
+			{
+				this->current_ *= a_super->get_scale();
+			}
+		}
 	}
 
 	//-------------------------------------------------------------------------
-	void reset(typename this_type::scale const& i_scale)
+	void reset(typename this_type::lerp const& i_scale)
 	{
 		this->scale_ = i_scale;
 		this->current_ = this->scale_.current();
@@ -54,6 +57,7 @@ public:
 		if (NULL != a_super)
 		{
 			this->current_ *= a_super->get_scale();
+			this->count_ = a_super->count_;
 		}
 	}
 
@@ -73,7 +77,7 @@ public:
 	}
 
 	void reset(
-		typename this_type::scale const&      i_scale,
+		typename this_type::lerp const&       i_scale,
 		typename this_type::shared_ptr const& i_super)
 	{
 		this->~time_frame();
@@ -81,85 +85,66 @@ public:
 	}
 
 	//-------------------------------------------------------------------------
-	typename this_type::scale::value_type get_scale()
+	t_scale get_scale()
 	{
-		this->update(0);
-		return this->current_;
-	}
-
-	//-------------------------------------------------------------------------
-	t_count get_count() const
-	{
-		return this->count_;
-	}
-
-	void update_count()
-	{
-		this->update(1);
-	}
-
-//.............................................................................
-private:
-	void construct(this_type* const i_super)
-	{
-		if (NULL == i_super)
-		{
-			this->count_ = 0;
-		}
-		else if (!i_super->find_super(*this))
-		{
-			this->current_ *= i_super->get_scale();
-			this->count_ = i_super->get_count();
-		}
-		else
-		{
-			PSYQ_ASSERT(false);
-			this->super_.reset();
-			this->count_ = 0;
-		}
-	}
-
-	void update(typename this_type::count const i_count)
-	{
+		// 上位instanceのscaleとcountを取得。
 		this_type* const a_super(this->super_.get());
 		float a_scale;
-		this_type::count a_count;
+		t_count a_count;
 		if (NULL != a_super)
 		{
 			a_scale = a_super->get_scale();
-			a_count = a_super->get_count();
+			a_count = a_super->count_;
 		}
 		else
 		{
 			a_scale = 1;
-			a_count = this->count_ + i_count;
+			a_count = this_type::frame_count();
 		}
 
+		// count値が異なっていたら更新する。
 		if (a_count != this->count_)
 		{
 			this->scale_.update(a_count - this->count_);
 			this->count_ = a_count;
 			this->current_ = this->scale_.current() * a_scale;
 		}
+		return this->current_;
 	}
 
+	//-------------------------------------------------------------------------
+	static t_count get_count()
+	{
+		return this_type::frame_count();
+	}
+
+	static t_count update_count()
+	{
+		return ++this_type::frame_count();
+	}
+
+//.............................................................................
+private:
 	bool find_super(this_type const& i_timer) const
 	{
-		if (&i_timer == this)
+		if (&i_timer != this)
 		{
-			return true;
+			this_type const* const a_super(this->super_.get());
+			return NULL != a_super? a_super->find_super(i_timer): false;
 		}
-		if (NULL == this->super_.get())
-		{
-			return false;
-		}
-		return this->super_->find_super(i_timer);
+		return true;
+	}
+
+	static t_count& frame_count()
+	{
+		static t_count s_count(0);
+		return s_count;
 	}
 
 //.............................................................................
 private:
 	typename this_type::shared_ptr super_;
-	typename this_type::scale      scale_;
+	typename this_type::lerp       scale_;
 	t_scale                        current_;
 	t_count                        count_;
 };

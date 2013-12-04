2cb628908db5238e1a90302c4cd6ecba36feec71
psychi <psychicphysical+github.com@googlemail.com>
2013-12-05 01:24:39 +0900

    psyq::type_hashの機能を、psyq::anyと統一した。

--- any.hpp
+++ any.hpp
@@ -29,6 +29,11 @@
 #define PSYQ_ANY_HPP_
 #include <memory>
 //#include "psyq/type_hash.hpp"
+#ifndef PSYQ_ANY_TYPE_HASH_RESERVED_COUNT
+/// 予約済みの型の識別値の数。
+#define PSYQ_ANY_TYPE_HASH_RESERVED_COUNT\
+    std::size_t(1 << (sizeof(std::size_t) * 8 - 1))
+#endif // !defined(PSYQ_ANY_TYPE_HASH_RESERVED_COUNT)
 
 namespace psyq
 {
@@ -49,10 +54,13 @@ class psyq::any
     public: typedef std::shared_ptr<self> shared_ptr; ///< self の保持子。
     public: typedef std::weak_ptr<self>   weak_ptr;   ///< self の監視子。
 
+    public: typedef std::size_t type_hash; ///< 型の識別値。
+
     /// @cond
     public: template<typename> class holder;
     /// @endcond
 
+    //-------------------------------------------------------------------------
     public: virtual ~any() {}
 
     /** @brief 保持してる値の型情報を取得する。
@@ -63,7 +71,7 @@ class psyq::any
     /** @brief 保持してる値の型の識別値を取得する。
         @return 保持してる値の型の識別値。
      */
-    public: virtual psyq::type_hash get_type_hash() const = 0;
+    public: virtual self::type_hash get_type_hash() const = 0;
 
     /** @brief 保持してる値の大きさを取得する。
         @return 保持してる値のbyte単位の大きさ。
@@ -73,7 +81,99 @@ class psyq::any
     /** @brief 保持してる値へのpointerを取得する。
         @return 保持してる値へのpointer。
      */
-    public: virtual void* get_pointer() = 0;
+    protected: virtual void* get_pointer() = 0;
+
+    /** @brief psyq::any が保持してる値へのpointerを取得する。
+        @tparam template_value 取得する値の型。
+        @retval !=nullptr thisが保持してる値へのpointer。
+        @retval ==nullptr
+            実際に保持してる値の型と template_value が異なっていた。
+     */
+    public: template<typename template_value>
+    template_value* get_value()
+    {
+        if (this->get_type_hash() != self::make_type_hash<template_value>())
+        {
+            return nullptr;
+        }
+        return static_cast<template_value*>(this->get_pointer());
+    }
+
+    /// @copydoc get_value(psyq::any* const)
+    public: template<typename template_value>
+    template_value const* get_value() const
+    {
+        return const_cast<self*>(in_any)->get_value<template_value>();
+    }
+
+    //-------------------------------------------------------------------------
+    /** @brief 型の識別値を取得する。
+        @tparam template_type 識別値を取得したい型。
+        @return 型の識別値。
+     */
+    public: template<typename template_type>
+    static self::type_hash make_type_hash()
+    {
+        return self::register_type_hash<
+            typename std::remove_cv<template_type>::type>(
+                PSYQ_ANY_TYPE_HASH_RESERVED_COUNT);
+    }
+
+    /** @brief 型の識別値を設定する。
+
+        型の識別値は、一度設定すると変更できない。
+
+        @tparam template_type 識別値を設定したい型。
+        @retval !=psyq::get_type_hash<void>() 型の識別値。
+        @retval ==psyq::get_type_hash<void>() 失敗。すでに設定されていた。
+     */
+    public: template<typename template_type>
+    static self::type_hash reserve_type_hash(self::type_hash const in_type)
+    {
+        typedef typename std::remove_cv<template_type>::type type;
+        if (in_type < PSYQ_ANY_TYPE_HASH_RESERVED_COUNT
+            && in_type == self::register_type_hash<type>(in_type))
+        {
+            return in_type;
+        }
+        return PSYQ_ANY_TYPE_HASH_RESERVED_COUNT;
+    }
+
+    /** @brief 型の識別値を登録する。
+
+        一度登録された型の識別値は変更されない。
+
+        @return 登録されている型の識別値。
+     */
+    private: template<typename template_type>
+    static self::type_hash register_type_hash(self::type_hash const in_type)
+    {
+        static auto const static_type(
+            in_type != PSYQ_ANY_TYPE_HASH_RESERVED_COUNT?
+                in_type: self::add_type_hash());
+        return static_type;
+    }
+
+    /** @brief void型の識別値は登録できない。
+        @return void型の識別値。
+     */
+    private: template<>
+    static self::type_hash register_type_hash<void>(self::type_hash const)
+    {
+        return PSYQ_ANY_TYPE_HASH_RESERVED_COUNT;
+    }
+
+    /** @brief 型の識別値を追加する。
+        @return 追加された型の識別値。
+     */
+    private: static self::type_hash add_type_hash()
+    {
+        static psyq::atomic_count
+            static_type(PSYQ_ANY_TYPE_HASH_RESERVED_COUNT);
+        auto const local_type(static_type.add());
+        PSYQ_ASSERT(0 < local_type);
+        return local_type;
+    }
 };
 
 //ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
@@ -105,9 +205,9 @@ class psyq::any::holder: public psyq::any
         return typeid(typename self::value_type);
     }
 
-    public: virtual psyq::type_hash get_type_hash() const
+    public: virtual typename super::type_hash get_type_hash() const
     {
-        return psyq::get_type_hash<typename self::value_type>();
+        return super::make_type_hash<typename self::value_type>();
     }
 
     public: virtual std::size_t get_size() const
@@ -115,68 +215,12 @@ class psyq::any::holder: public psyq::any
         return sizeof(typename self::value_type);
     }
 
-    public: virtual void* get_pointer()
+    protected: virtual void* get_pointer()
     {
         return &this->value;
     }
 
     public: typename self::value_type value; ///< 保持してる値。
 };
-
-//ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
-namespace psyq
-{
-    /** @brief psyq::any が保持してる値を取得する。
-        @tparam template_value 取得する値の型。
-        @param[in] in_any 値を保持してるinstance。
-        @return
-            in_any が保持してる値への参照。
-            ただし in_any が実際に保持してる値の型と、
-            template_value が異なる場合はassertする。
-     */
-    template<typename template_value>
-    template_value& any_cast(psyq::any& in_any)
-    {
-        if (in_any.get_type_hash() != psyq::get_type_hash<template_value>())
-        {
-            PSYQ_ASSERT(false);
-            //throw std::basic_cast; // 例外は使いたくない。
-        }
-        return *static_cast<template_value*>(in_any.get_pointer());
-    }
-
-    /// @copydoc any_cast(psyq::any&)
-    template<typename template_value>
-    template_value const& any_cast(psyq::any const& in_any)
-    {
-        return psyq::any_cast(const_cast<psyq::any&>(in_any));
-    }
-
-    /** @brief psyq::any が保持してる値へのpointerを取得する。
-        @tparam template_value 取得する値の型。
-        @param[in] in_any 値を保持してるinstance。
-        @retval !=nullptr in_any が保持してる値へのpointer。
-        @retval ==nullptr
-            in_any がnullptrだったか,
-            in_any が実際に保持してる値の型と template_value が異なっていた。
-     */
-    template<typename template_value>
-    template_value* any_cast(psyq::any* const in_any)
-    {
-        if (in_any == nullptr
-            || in_any->get_type_hash() != psyq::get_type_hash<template_value>())
-        {
-            return nullptr;
-        }
-        return static_cast<template_value*>(in_any->get_pointer());
-    }
-
-    /// @copydoc any_cast(psyq::any* const)
-    template<typename template_value>
-    template_value const* any_cast(psyq::any const* const in_any)
-    {
-        return psyq::any_cast<template_value>(const_cast<psyq::any*>(in_any));
-    }
-}
 
 #endif // !defined(PSYQ_ANY_HPP_)

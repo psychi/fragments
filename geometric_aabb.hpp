4a516f3f4daa6f03cd8cfb32f048bca545bb585b
psychi <psychicphysical+github.com@googlemail.com>
2014-01-18 21:41:52 +0900

    psyq::ray::triangleクラスを実装。半線分と衝突する三角形。 "geometric_utility.hpp"を、以下のファイルに分解した。   - "geometric_vector.hpp"   - "geometric_shape.hpp"   - "geometric_aabb.hpp"

---
+++ geometric_aabb.hpp
@@ -0,0 +1,460 @@
+﻿#ifndef PSYQ_GEOMETIC_AABB_HPP_
+#define PSYQ_GEOMETIC_AABB_HPP_
+
+namespace psyq
+{
+    /// @cond
+    template<typename> class geometric_aabb;
+    /// @endcond
+}
+
+//ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+/** @brief axis-aligned-bounding-box. 座標軸に平行な直方体。
+    @tparam template_vector @copydoc geometric_aabb::vector
+ */
+template<typename template_vector>
+class psyq::geometric_aabb
+{
+    /// *thisの型。
+    private: typedef psyq::geometric_aabb<template_vector> self;
+
+    /// @copydoc psyq::geometric_vector::type
+    public: typedef template_vector vector;
+
+    /** @brief AABBを構築する。
+        @param[in] in_min AABBの最小座標。
+        @param[in] in_max AABBの最大座標。
+     */
+    public: geometric_aabb(
+        template_vector const& in_min,
+        template_vector const& in_max)
+    :
+        min_(in_min),
+        max_(in_max)
+    {
+        PSYQ_ASSERT(
+            psyq::geometric_vector_element(in_min, 0)
+            <= psyq::geometric_vector_element(in_max, 0));
+        PSYQ_ASSERT(
+            psyq::geometric_vector_element(in_min, 1)
+            <= psyq::geometric_vector_element(in_max, 1));
+        PSYQ_ASSERT(
+            psyq::geometric_vector_element(in_min, 2)
+            <= psyq::geometric_vector_element(in_max, 2));
+    }
+
+    /** @brief AABBの最小座標を取得する。
+        @return AABBの最小座標。
+     */
+    public: template_vector const& get_min() const
+    {
+        return this->min_;
+    }
+
+    /** @brief AABBの最大座標を取得する。
+        @return AABBの最大座標。
+     */
+    public: template_vector const& get_max() const
+    {
+        return this->max_;
+    }
+
+    /** @brief 2つのAABBが衝突しているか判定する。
+        @param[in] in_aabb0 AABB#0
+        @param[in] in_aabb1 AABB#1
+        @retval true  衝突している。
+        @retval false 衝突してない。
+     */
+    public: static bool detect_collision(
+        self const& in_aabb0,
+        self const& in_aabb1)
+    {
+        auto const local_diff0(in_aabb0.get_min() - in_aabb1.get_max());
+        auto const local_diff1(in_aabb1.get_min() - in_aabb0.get_max());
+#if 1
+        return psyq::geometric_vector_element(local_diff0, 0) < 0
+            && psyq::geometric_vector_element(local_diff0, 1) < 0
+            && psyq::geometric_vector_element(local_diff0, 2) < 0
+            && psyq::geometric_vector_element(local_diff1, 0) < 0
+            && psyq::geometric_vector_element(local_diff1, 1) < 0
+            && psyq::geometric_vector_element(local_diff1, 2) < 0;
+#else
+        // 浮動小数点の符号ビットがすべてマイナスか判定する。
+        return (
+            psyq::internal::get_float_bit_value(
+                psyq::geometric_vector_element(local_diff0, 0))
+            && psyq::internal::get_float_bit_value(
+                psyq::geometric_vector_element(local_diff0, 1))
+            && psyq::internal::get_float_bit_value(
+                psyq::geometric_vector_element(local_diff0, 2))
+            && psyq::internal::get_float_bit_value(
+                psyq::geometric_vector_element(local_diff1, 0))
+            && psyq::internal::get_float_bit_value(
+                psyq::geometric_vector_element(local_diff1, 1))
+            && psyq::internal::get_float_bit_value(
+                psyq::geometric_vector_element(local_diff1, 2))) >> 31;
+#endif
+    }
+
+    private: template_vector min_; ///< AABBの最小座標。
+    private: template_vector max_; ///< AABBの最大座標。
+};
+
+//ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+namespace psyq
+{
+    //-------------------------------------------------------------------------
+    /** @brief 任意の幾何形状からAABBを構築するためのクラス。
+        @tparam template_vector @copydoc psyq::geometric_shape_aabb::vector
+        @tparam template_shape  @copydoc psyq::geometric_shape_aabb::shape
+     */
+    template<typename template_vector, typename template_shape>
+    struct geometric_shape_aabb
+    {
+        /// AABBの座標を表す幾何ベクトルの型。
+        typedef template_vector vector;
+
+        /// AABBを構築する幾何形状の型。
+        typedef template_shape shape;
+
+        /** @brief 任意の幾何形状からAABBを構築する。
+
+            ただし、ここではインターフェイスの定義しか行わない。
+            テンプレート特殊化した geometric_shape_aabb で実装を行うこと。
+
+            @param[in] in_shape AABBを構築する幾何形状。
+            @return 幾何形状のAABB。
+         */
+        static psyq::geometric_aabb<template_vector> make(
+            template_shape const& in_shape);
+    };
+
+    //-------------------------------------------------------------------------
+    /** @brief AABBを構築するためのクラス。
+        @tparam template_vector @copydoc psyq::geometric_shape_aabb::vector
+     */
+    template<typename template_vector>
+    struct geometric_shape_aabb<
+        template_vector, psyq::geometric_aabb<template_vector>>
+    {
+        /// @copydoc geometric_shape_aabb::vector
+        typedef template_vector vector;
+
+        /// AABBの型。
+        typedef psyq::geometric_aabb<template_vector> shape;
+
+        static psyq::geometric_aabb<template_vector> make(
+            psyq::geometric_aabb<template_vector> const& in_aabb)
+        {
+            return in_aabb;
+        }
+
+        /** @brief 2つの点を含む最小のAABBを構築する。
+            @param[in] in_point_a 点Aの座標。
+            @param[in] in_point_b 点Bの座標。
+            @return 点Aと点Bを包むAABB。
+         */
+        static psyq::geometric_aabb<template_vector> make(
+            template_vector const& in_point_a,
+            template_vector const& in_point_b)
+        {
+            auto const local_x(
+                std::minmax(
+                    psyq::geometric_vector_element(in_point_a, 0),
+                    psyq::geometric_vector_element(in_point_b, 0)));
+            auto const local_y(
+                std::minmax(
+                    psyq::geometric_vector_element(in_point_a, 1),
+                    psyq::geometric_vector_element(in_point_b, 1)));
+            auto const local_z(
+                std::minmax(
+                    psyq::geometric_vector_element(in_point_a, 2),
+                    psyq::geometric_vector_element(in_point_b, 2)));
+            return psyq::geometric_aabb<template_vector>(
+                psyq::geometric_vector<template_vector>::make(
+                    local_x.first, local_y.first, local_z.first),
+                psyq::geometric_vector<template_vector>::make(
+                    local_x.second, local_y.second, local_z.second));
+        }
+    };
+
+    //-------------------------------------------------------------------------
+    /** @brief 球からAABBを構築するためのクラス。
+        @tparam template_vector @copydoc psyq::geometric_shape_aabb::vector
+     */
+    template<typename template_vector>
+    struct geometric_shape_aabb<
+        template_vector, psyq::geometric_sphere<template_vector>>
+    {
+        /// @copydoc geometric_shape_aabb::vector
+        typedef template_vector vector;
+
+        /// AABBを構築する球の型。
+        typedef psyq::geometric_sphere<template_vector> shape;
+
+        /** @brief 球のAABBを構築する。
+            @param[in] in_sphere AABBを構築する球。
+            @return 球のAABB。
+         */
+        static psyq::geometric_aabb<template_vector> make(
+            psyq::geometric_sphere<template_vector> const& in_sphere)
+        {
+            auto const local_extent(
+                psyq::geometric_vector<template_vector>::make(
+                    in_sphere.get_radius()));
+            return psyq::geometric_aabb<template_vector>(
+                in_sphere.get_center() - local_extent,
+                in_sphere.get_center() + local_extent);
+        }
+    };
+
+    //-------------------------------------------------------------------------
+    /** @brief 線分からAABBを構築するためのクラス。
+        @tparam template_vector @copydoc psyq::geometric_shape_aabb::vector
+     */
+    template<typename template_vector>
+    struct geometric_shape_aabb<
+        template_vector, psyq::geometric_segment<template_vector>>
+    { 
+        /// @copydoc geometric_shape_aabb::vector
+       typedef template_vector vector;
+
+        /// AABBを構築する線分の型。
+        typedef psyq::geometric_segment<template_vector> shape;
+
+        /** @brief 線分のAABBを構築する。
+            @param[in] in_segment AABBを構築する線分。
+            @return 線分のAABB。
+         */
+        static psyq::geometric_aabb<template_vector> make(
+            psyq::geometric_segment<template_vector> const& in_segment)
+        {
+            auto const local_end(this->get_origin() + this->get_direction());
+            typedef typename psyq::geometric_vector<template_vector>::element
+                vector_element;
+
+            // X座標の範囲を決定する。
+            auto const local_direction_x(
+                psyq::geometric_vector_element(in_segment.get_direction(), 0));
+            auto const local_origin_x(
+                psyq::geometric_vector_element(in_segment.get_origin(), 0));
+            auto const local_end_x(
+                psyq::geometric_vector_element(local_end, 0));
+            vector_element local_min_x;
+            vector_element local_max_x;
+            if (local_direction_x < 0)
+            {
+                local_min_x = local_end_x;
+                local_max_x = local_origin_x;
+            }
+            else
+            {
+                local_min_x = local_origin_x;
+                local_max_x = local_end_x;
+            }
+
+            // Y座標の範囲を決定する。
+            auto const local_direction_y(
+                psyq::geometric_vector_element(in_segment.get_direction(), 1));
+            auto const local_origin_y(
+                psyq::geometric_vector_element(in_segment.get_origin(), 1));
+            auto const local_end_y(
+                psyq::geometric_vector_element(local_end, 1));
+            vector_element local_min_y;
+            vector_element local_max_y;
+            if (local_direction_y < 0)
+            {
+                local_min_y = local_end_y;
+                local_max_y = local_origin_y;
+            }
+            else
+            {
+                local_min_y = local_origin_y;
+                local_max_y = local_end_y;
+            }
+
+            // Z座標の範囲を決定する。
+            auto const local_direction_z(
+                psyq::geometric_vector_element(in_segment.get_direction(), 2));
+            auto const local_origin_z(
+                psyq::geometric_vector_element(in_segment.get_origin(), 2));
+            auto const local_end_z(
+                psyq::geometric_vector_element(local_end, 2));
+            vector_element local_min_z;
+            vector_element local_max_z;
+            if (local_direction_z < 0)
+            {
+                local_min_z = local_end_z;
+                local_max_z = local_origin_z;
+            }
+            else
+            {
+                local_min_z = local_origin_z;
+                local_max_z = local_end_z;
+            }
+
+            return psyq::geometric_aabb<template_vector>(
+                psyq::geometric_vector<template_vector>::make(
+                    local_min_x, local_min_y, local_min_z),
+                psyq::geometric_vector<template_vector>::make(
+                    local_max_x, local_max_y, local_max_z));
+        }
+    };
+
+    //-------------------------------------------------------------------------
+    /** @brief 半線分からAABBを構築するためのクラス。
+        @tparam template_vector @copydoc psyq::geometric_shape_aabb::vector
+     */
+    template<typename template_vector>
+    struct geometric_shape_aabb<
+        template_vector, psyq::geometric_ray<template_vector>>
+    {
+        /// @copydoc geometric_shape_aabb::vector
+        typedef template_vector vector;
+
+        /// AABBを構築する半線分の型。
+        typedef psyq::geometric_ray<template_vector> shape;
+
+        /** @brief 半線分のAABBを構築する。
+            @param[in] in_ray AABBを構築する半線分。
+            @return 半線分のAABB。
+         */
+        static psyq::geometric_aabb<template_vector> make(
+            psyq::geometric_ray<template_vector> const& in_ray)
+        {
+            typedef typename psyq::geometric_vector<template_vector>::element
+                vector_element;
+
+            // X座標の範囲を決定する。
+            auto const local_direction_x(
+                psyq::geometric_vector_element(in_ray.get_direction(), 0));
+            auto const local_origin_x(
+                psyq::geometric_vector_element(in_ray.get_origin(), 0));
+            vector_element local_min_x;
+            vector_element local_max_x;
+            if (local_direction_x < 0)
+            {
+                local_min_x = -(std::numeric_limits<vector_element>::max)();
+                local_max_x = local_origin_x;
+            }
+            else if (0 < local_direction_x)
+            {
+                local_min_x = local_origin_x;
+                local_max_x = (std::numeric_limits<vector_element>::max)();
+            }
+            else
+            {
+                local_min_x = local_origin_x;
+                local_max_x = local_origin_x;
+            }
+
+            // Y座標の範囲を決定する。
+            auto const local_direction_y(
+                psyq::geometric_vector_element(in_ray.get_direction(), 1));
+            auto const local_origin_y(
+                psyq::geometric_vector_element(in_ray.get_origin(), 1));
+            vector_element local_min_y;
+            vector_element local_max_y;
+            if (local_direction_y < 0)
+            {
+                local_min_y = -(std::numeric_limits<vector_element>::max)();
+                local_max_y = local_origin_y;
+            }
+            else if (0 < local_direction_y)
+            {
+                local_min_y = local_origin_y;
+                local_max_y = (std::numeric_limits<vector_element>::max)();
+            }
+            else
+            {
+                local_min_y = local_origin_y;
+                local_max_y = local_origin_y;
+            }
+
+            // Z座標の範囲を決定する。
+            auto const local_direction_z(
+                psyq::geometric_vector_element(in_ray.get_direction(), 2));
+            auto const local_origin_z(
+                psyq::geometric_vector_element(in_ray.get_origin(), 2));
+            vector_element local_min_z;
+            vector_element local_max_z;
+            if (local_direction_z < 0)
+            {
+                local_min_z = -(std::numeric_limits<vector_element>::max)();
+                local_max_z = local_origin_z;
+            }
+            else if (0 < local_direction_z)
+            {
+                local_min_z = local_origin_z;
+                local_max_z = (std::numeric_limits<vector_element>::max)();
+            }
+            else
+            {
+                local_min_z = local_origin_z;
+                local_max_z = local_origin_z;
+            }
+
+            return psyq::geometric_aabb<template_vector>(
+                psyq::geometric_vector<template_vector>::make(
+                    local_min_x, local_min_y, local_min_z),
+                psyq::geometric_vector<template_vector>::make(
+                    local_max_x, local_max_y, local_max_z));
+        }
+    };
+
+    //-------------------------------------------------------------------------
+    /** @brief 直方体からAABBを構築するためのクラス。
+        @tparam template_vector @copydoc psyq::geometric_shape_aabb::vector
+     */
+    template<typename template_vector>
+    struct geometric_shape_aabb<
+        template_vector, psyq::geometric_cuboid<template_vector>>
+    {
+        /// @copydoc geometric_shape_aabb::vector
+        typedef template_vector vector;
+
+        /// AABBを構築する直方体の型。
+        typedef psyq::geometric_cuboid<template_vector> shape;
+
+        /** @brief 直方体のAABBを構築する。
+            @param[in] in_cuboid AABBを構築する直方体。
+            @return 直方体のAABB。
+         */
+        static psyq::geometric_aabb<template_vector> make(
+            psyq::geometric_cuboid<template_vector> const& in_cuboid)
+        {
+            auto const local_abs_vector(
+                [](template_vector const& in_vector)->template_vector
+                {
+                    auto const local_x(
+                        psyq::geometric_vector_element(in_vector, 0));
+                    auto const local_y(
+                        psyq::geometric_vector_element(in_vector, 1));
+                    auto const local_z(
+                        psyq::geometric_vector_element(in_vector, 2));
+                    return psyq::geometric_vector<template_vector>::make(
+                        local_x < 0? -local_x: local_x,
+                        local_y < 0? -local_y: local_y,
+                        local_z < 0? -local_z: local_z);
+                });
+            auto const local_half_diagonal(
+                local_abs_vector(
+                    in_cuboid.get_axis_x()
+                    * psyq::geometric_vector_element(
+                        in_cuboid.get_extent(), 0))
+                + local_abs_vector(
+                    in_cuboid.get_axis_y()
+                    * psyq::geometric_vector_element(
+                        in_cuboid.get_extent(), 1))
+                + local_abs_vector(
+                    in_cuboid.get_axis_z()
+                    * psyq::geometric_vector_element(
+                        in_cuboid.get_extent(), 2)));
+            return psyq::geometric_aabb<template_vector>(
+                in_cuboid.get_center() - local_half_diagonal,
+                in_cuboid.get_center() + local_half_diagonal);
+        }
+    };
+}
+
+#endif // !defined(PSYQ_GEOMETIC_AABB_HPP_)
---
+++ geometric_vector.hpp
@@ -0,0 +1,245 @@
+﻿#ifndef PSYQ_GEOMETIC_VECTOR_HPP_
+#define PSYQ_GEOMETIC_VECTOR_HPP_
+
+#ifndef PSYQ_NEARLY_EQUAL_EPSILON_MAG_DEFAULT
+#define PSYQ_NEARLY_EQUAL_EPSILON_MAG_DEFAULT 1
+#endif
+
+namespace psyq
+{
+    /// @cond
+    template<typename> struct geometric_vector;
+    /// @endcond
+
+    /** @brief 2つの浮動小数点値がほぼ等値か比較する。
+        @param[in] in_left_value  比較する浮動小数点値の左辺値。
+        @param[in] in_right_value 比較する浮動小数点値の右辺値。
+        @param[in] in_epsilon_mag 誤差の範囲に使うエプシロン値の倍率。
+        @retval true  ほぼ等値だった。
+        @retval false 等値ではなかった。
+     */
+    template<typename template_value>
+    bool is_nearly_equal(
+        template_value const in_left_value,
+        template_value const in_right_value,
+        unsigned const       in_epsilon_mag
+            = PSYQ_NEARLY_EQUAL_EPSILON_MAG_DEFAULT)
+    {
+        auto const local_epsilon(
+            std::numeric_limits<template_value>::epsilon() * in_epsilon_mag);
+        auto const local_diff(in_left_value - in_right_value);
+        return -local_epsilon <= local_diff && local_diff <= local_epsilon;
+    }
+}
+
+//ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+/** @brief 幾何ベクトルの型特性。
+    @tparam template_vector @copydoc psyq::geometric_vector::type
+ */
+template<typename template_vector>
+struct psyq::geometric_vector
+{
+    /** @brief 操作する幾何ベクトルの型。
+
+        以下の条件を満たしていること。
+        - 仮想関数を持たないこと。
+        - メンバ変数として、3つ以上の要素を持つこと。
+        - 要素は、すべてfloat型であること。
+        - 要素は、連続したメモリに配置されていること。
+        - 最初の要素の配置位置は、template_vectorの先頭と一致すること。
+
+        上記をいずれかひとつでも満たさない場合は、
+        以下の実装をテンプレート特殊化して用意すること。
+        - psyq::geometric_vector
+        - psyq::geometric_vector_element()
+        - psyq::geometric_vector_cross()
+
+        以下の条件は、必ず満たしていること。
+        - copyコンストラクタとcopy代入演算子が使える。
+        - 以下に相当するコンストラクタを持つ。
+          - template_vector::template_vector(element, element, element)
+        - 以下に相当する二項演算子が使える。
+          - template_vector operator+(template_vector, template_vector)
+          - template_vector operator-(template_vector, template_vector)
+          - template_vector operator*(template_vector, template_vector)
+          - template_vector operator*(template_vector, element)
+          - template_vector operator/(template_vector, template_vector)
+          - template_vector operator/(template_vector, element)
+     */
+    typedef template_vector type;
+
+    /// 幾何ベクトルの要素の型。
+    typedef float element;
+
+    enum: unsigned
+    {
+        SIZE = 3 ///< 幾何ベクトルが持つ要素の数。
+    };
+
+    /// 幾何ベクトルを作る。
+    static template_vector make(element const in_element)
+    {
+        return template_vector(in_element, in_element, in_element);
+    }
+
+    /// 幾何ベクトルを作る。
+    static template_vector make(
+        element const in_element0,
+        element const in_element1,
+        element const in_element2)
+    {
+        return template_vector(in_element0, in_element1, in_element2);
+    }
+};
+
+//ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+namespace psyq
+{
+    /** @brief 幾何ベクトルの要素から値を取得する。
+        @tparam template_vector @copydoc psyq::geometric_vector::type
+        @param[in] in_vector        要素の値を取得する幾何ベクトル。
+        @param[in] in_element_index 取得する要素のインデックス番号。
+        @return template_vector の要素の値。
+     */
+    template<typename template_vector>
+    typename psyq::geometric_vector<template_vector>::element
+    geometric_vector_element(
+        template_vector const& in_vector,
+        unsigned const         in_element_index)
+    {
+        PSYQ_ASSERT(
+            in_element_index < psyq::geometric_vector<template_vector>::SIZE);
+        typedef typename psyq::geometric_vector<template_vector>::element const
+            vector_element;
+        auto const local_element(
+            reinterpret_cast<vector_element*>(&in_vector) + in_element_index);
+        return *local_element;
+    }
+
+    /** @brief 幾何ベクトルの要素に値を設定する。
+        @tparam template_vector @copydoc psyq::geometric_vector::type
+        @param[in,out] io_vector        要素に値を設定する幾何ベクトル。
+        @param[in]     in_element_index 設定する要素のインデックス番号。
+        @param[in]     in_element_value 設定する要素の値。
+        @return template_vector の要素に設定した値。
+     */
+    template<typename template_vector>
+    typename psyq::geometric_vector<template_vector>::element
+    geometric_vector_element(
+        template_vector& io_vector,
+        unsigned const   in_element_index,
+        typename psyq::geometric_vector<template_vector>::element const
+            in_element_value)
+    {
+        PSYQ_ASSERT(
+            in_element_index < psyq::geometric_vector<template_vector>::SIZE);
+        typedef typename psyq::geometric_vector<template_vector>::element
+            vector_element;
+        auto const local_element(
+            reinterpret_cast<vector_element*>(&io_vector) + in_element_index);
+        *local_element = in_element_value;
+        return in_element_value;
+    }
+
+    /** @brief 幾何ベクトルの長さの2乗を算出する。
+        @tparam template_vector @copydoc psyq::geometric_vector::type
+        @param[in] in_vector 長さの2乗を算出する幾何ベクトル。
+        @return 幾何ベクトルの長さの2乗。
+     */
+    template<typename template_vector>
+    typename psyq::geometric_vector<template_vector>::element
+    geometric_vector_squared_length(template_vector const& in_vector)
+    {
+        auto const local_element(
+            psyq::geometric_vector_element(in_vector, 0));
+        auto local_squared_length(local_element * local_element);
+        for (
+            unsigned i(1);
+            i < psyq::geometric_vector<template_vector>::SIZE;
+            ++i)
+        {
+            auto const local_element(
+                psyq::geometric_vector_element(in_vector, i));
+            local_squared_length += local_element * local_element;
+        }
+        return local_squared_length;
+    }
+
+    /** @brief 幾何ベクトルの長さを算出する。
+        @tparam template_vector @copydoc psyq::geometric_vector::type
+        @param[in] in_vector 長さを算出する幾何ベクトル。
+        @return 幾何ベクトルの長さ。
+     */
+    template<typename template_vector>
+    typename psyq::geometric_vector<template_vector>::element
+    geometric_vector_length(template_vector const& in_vector)
+    {
+        return std::sqrt(psyq::geometric_vector_squared_length(in_vector));
+    }
+
+    /** @brief 幾何ベクトルが正規化されているか判定する。
+        @tparam template_vector @copydoc psyq::geometric_vector::type
+        @param[in] in_vector 判定する幾何ベクトル。
+        @retval true  幾何ベクトルは正規化されている。
+        @retval false 幾何ベクトルは正規化されていない。
+     */
+    template<typename template_vector>
+    bool geometric_vector_is_normalized(template_vector const& in_vector)
+    {
+        return psyq::is_nearly_equal(
+            psyq::geometric_vector_squared_length(in_vector),
+            typename psyq::geometric_vector<template_vector>::element(1));
+    }
+
+    /** @brief 2つの幾何ベクトルの内積を算出する。
+        @tparam template_vector @copydoc psyq::geometric_vector::type
+        @param[in] in_left  左辺の幾何ベクトル。
+        @param[in] in_right 右辺の幾何ベクトル。
+        @return 2つの幾何ベクトルの内積。
+     */
+    template<typename template_vector>
+    typename psyq::geometric_vector<template_vector>::element
+    geometric_vector_dot(
+        template_vector const& in_left,
+        template_vector const& in_right)
+    {
+        auto local_dot(
+            psyq::geometric_vector_element(in_left, 0)
+            * psyq::geometric_vector_element(in_right, 0));
+        for (
+            unsigned i(1);
+            i < psyq::geometric_vector<template_vector>::SIZE;
+            ++i)
+        {
+            local_dot +=
+                psyq::geometric_vector_element(in_left, i)
+                * psyq::geometric_vector_element(in_right, i);
+        }
+        return local_dot;
+    }
+
+    /** @brief 2つの幾何ベクトルの外積を算出する。
+        @tparam template_vector @copydoc psyq::geometric_vector::type
+        @param[in] in_left  左辺の幾何ベクトル。
+        @param[in] in_right 右辺の幾何ベクトル。
+        @return 2つの幾何ベクトルの外積。
+     */
+    template<typename template_vector>
+    template_vector geometric_vector_cross(
+        template_vector const& in_left,
+        template_vector const& in_right)
+    {
+        auto const local_left0(psyq::geometric_vector_element(in_left, 0));
+        auto const local_left1(psyq::geometric_vector_element(in_left, 1));
+        auto const local_left2(psyq::geometric_vector_element(in_left, 2));
+        auto const local_right0(psyq::geometric_vector_element(in_right, 0));
+        auto const local_right1(psyq::geometric_vector_element(in_right, 1));
+        auto const local_right2(psyq::geometric_vector_element(in_right, 2));
+        return psyq::geometric_vector<template_vector>::make(
+            local_left1 * local_right2 - local_left2 * local_right1,
+            local_left2 * local_right0 - local_left0 * local_right2,
+            local_left0 * local_right1 - local_left1 * local_right0);
+    }
+};
+
+#endif // !defined(PSYQ_GEOMETIC_VECTOR_HPP_)

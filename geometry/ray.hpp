019eea4f4c5265a3e858a24670dff9ad56bdf5f2
psychi <psychicphysical+github.com@googlemail.com>
2014-11-18 11:46:06 +0900

    psyq::geometry::barycentric_triangleを実装中。

--- geometry/ray.hpp
+++ geometry/ray.hpp
@@ -71,7 +71,7 @@ class psyq::geometry::barycentric_triangle
     public: typedef template_coordinate coordinate;
 
     /// @cond
-    public: class ray_collision;
+    public: class line_collision;
     /// @endcond
 
     //-------------------------------------------------------------------------
@@ -102,6 +102,11 @@ class psyq::geometry::barycentric_triangle
             psyq::geometry::vector::dot(local_edge_2, local_ny));
         PSYQ_ASSERT(local_dot_1 != 0);
         PSYQ_ASSERT(local_dot_2 != 0);
+        return this_type(
+            this_type::coordinate::make(in_vertex_0),
+            local_normal,
+            local_nx / local_dot_1,
+            local_ny / local_dot_2);
 #else
         auto const local_cross_1(
             psyq::geometry::vector::cross_3d(in_vertex_2, in_vertex_0));
@@ -167,27 +172,27 @@ class psyq::geometry::barycentric_triangle
 }; // class psyq::geometry::ray::barycentric_triangle
 
 //ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
-/** @brief 三角形と半直線の衝突判定。
+/** @brief 三角形と直線の衝突判定。
     @tparam template_coordinate @copydoc psyq::geometry::plane::coordinate
  */
 template<typename template_coordinate>
-class psyq::geometry::barycentric_triangle<template_coordinate>::ray_collision
+class psyq::geometry::barycentric_triangle<template_coordinate>::line_collision
 {
     /// thisが指す値の型。
-    private: typedef ray_collision this_type;
+    private: typedef line_collision this_type;
 
     /// @copydoc psyq::geometry::point::coordinate
     public: typedef template_coordinate coordinate;
 
     //-------------------------------------------------------------------------
-    /** @brief 三角形と半直線の衝突判定を構築する。
+    /** @brief 三角形と直線の衝突判定を構築する。
 
         以下の web page を参考にした。
         http://d.hatena.ne.jp/ototoi/20050320/p1
 
         衝突した場合、衝突位置は以下の式で算出できる。
         @code
-        in_ray.make_point(this->get_line_position())
+        in_line.make_point(this->get_line_position())
         @endcode
 
         また頂点毎に法線がある場合、以下の式で衝突位置の法線が算出できる。
@@ -197,71 +202,106 @@ class psyq::geometry::barycentric_triangle<template_coordinate>::ray_collision
         + normal_2 * this->get_v_position()
         @endcode
 
-        @param[in] in_triangle 衝突判定の左辺となる三角形。
-        @param[in] in_ray      衝突判定の右辺となる半直線。
-        @param[in] in_ray_end  半直線の衝突区間の終点位置。
-        @param[in] in_epsilon  計算機epsilon値。 
+        @param[in] in_triangle   衝突判定の左辺となる三角形。
+        @param[in] in_both_faces 三角形の両面とも衝突するかどうか。
+        @param[in] in_line       衝突判定の右辺となる直線。
+        @param[in] in_line_begin 直線の衝突区間の始点位置。
+        @param[in] in_line_end   直線の衝突区間の終点位置。
+        @param[in] in_epsilon    平行判定で許容する誤差。
     */
-    public: static this_type make(
+    public: static bool make(
         psyq::geometry::barycentric_triangle<template_coordinate> const& in_triangle,
-        psyq::geometry::ray<template_coordinate> const& in_ray,
-        typename this_type::coordinate::element const in_ray_end,
+        bool const in_both_faces,
+        psyq::geometry::line<template_coordinate> const& in_line,
+        typename this_type::coordinate::element const in_line_begin,
+        typename this_type::coordinate::element const in_line_end,
         typename this_type::coordinate::element const in_epsilon)
     {
-        auto const local_nv(
-            -psyq::geometry::vector::dot(
-                in_ray.direction_.get_unit(), in_triangle.get_normal()));
-        if (local_nv <= in_epsilon)
+        PSYQ_ASSERT(in_line_begin <= in_line_end);
+        PSYQ_ASSERT(0 <= in_epsilon);
+
+        // 三角形の方向と直線の方向が平行ではないか判定する。
+        auto const local_direction_dot(
+            psyq::geometry::vector::dot(
+                in_line.direction_.get_unit(), in_triangle.get_normal()));
+        if (-in_epsilon <= local_direction_dot
+            && (!in_both_faces || local_direction_dot <= in_epsilon))
         {
             return false;
         }
 
+        // 三角形のある平面と直線が交わる位置を算出する。
         auto const local_origin_difference(
-            in_ray.origin_.get_position() - in_triangle.get_origin());
+            in_line.origin_.get_position() - in_triangle.get_origin());
         auto const local_line_position(
-            psyq::geometry::vector::dot(local_origin_difference, in_triangle.get_normal())
-            / local_nv);
-        if (local_line_position < 0 || in_ray_end < local_line_position)
+            psyq::geometry::vector::dot(
+                local_origin_difference, in_triangle.get_normal())
+            / -local_direction_dot);
+        if (local_line_position < in_line_begin || in_line_end < local_line_position)
         {
             return false;
         }
 
         auto const local_cross_position(
-            in_ray.direction_.get_unit() * local_line_position
+            in_line.direction_.get_unit() * local_line_position
             + local_origin_difference);
         auto const local_u_position(
             psyq::geometry::vector::dot(
                 local_cross_position, in_triangle.get_binormal_u()));
-        if (local_u_position < in_epsilon)
+        if (local_u_position < 0)
         {
             return false;
         }
         auto const local_v_position(
             psyq::geometry::vector::dot(
                 local_cross_position, in_triangle.get_binormal_v()));
-        if (local_v_position < in_epsilon
-            || 1 - in_epsilon < local_u_position + local_v_position)
+        if (local_v_position < 0 || 1 < local_u_position + local_v_position)
         {
             return false;
         }
+        /*
         return this_type(
             local_cross_position,
             local_line_position,
             local_u_position,
-            local_v_position);
+            local_v_position,
+            local_direction_dot);
+            */
+        return true;
+    }
+
+    public: bool detect() const PSYQ_NOEXCEPT
+    {
+        return 0 <= this->get_u_position()
+            && 0 <= this->get_v_position()
+            && this->get_u_position() + this->get_v_position() <= 1;
+    }
+
+    public: typename this_type::coordinate::element get_u_position()
+    const PSYQ_NOEXCEPT
+    {
+        return this->u_position_;
+    }
+
+    public: typename this_type::coordinate::element get_v_position()
+    const PSYQ_NOEXCEPT
+    {
+        return this->v_position_;
     }
 
     //-------------------------------------------------------------------------
-    private: ray_collision(
+    private: line_collision(
         typename this_type::coordinate::vector const& in_cross_position,
         typename this_type::coordinate::element const in_line_position,
         typename this_type::coordinate::element const in_u_position,
-        typename this_type::coordinate::element const in_v_position)
+        typename this_type::coordinate::element const in_v_position,
+        typename this_type::coordinate::element const in_direction_dot)
     :
     cross_position_(in_cross_position),
     line_position_(in_line_position),
     u_position_(in_u_position),
-    v_position_(in_v_position)
+    v_position_(in_v_position),
+    direction_dot_(in_direction_dot)
     {}
 
     //-------------------------------------------------------------------------
@@ -272,7 +312,9 @@ class psyq::geometry::barycentric_triangle<template_coordinate>::ray_collision
     private: typename this_type::coordinate::element u_position_;
     /// 三角形の重心座標Vでの、交点の位置。
     private: typename this_type::coordinate::element v_position_;
+    /// 三角形の法線と直線の方向の内積。
+    private: typename this_type::coordinate::element direction_dot_;
 
-}; // psyq::geometry::barycentric_triangle::ray_collision
+}; // psyq::geometry::barycentric_triangle::line_collision
 
 #endif // PSYQ_GEOMETRY_RAY_HPP_

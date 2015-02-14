a8ed7f0a284dcb6865e9ce22c6d67712f9318766
psychi <psychicphysical+github.com@googlemail.com>
2015-02-15 05:25:18 +0900

    psyq::string::storageに、std::basic_string互換の関数を実装中。

--- string/storage.hpp
+++ string/storage.hpp
@@ -107,12 +107,13 @@ class psyq::string::_private::storage_base
         this->storage_[0] = 0;
     }
 
-    /** @brief 文字列をコピーする。
+    /** @brief 文字列をコピー構築する。
         @param[in] in_string コピー元となる文字列。
      */
     protected: storage_base(this_type const& in_string) PSYQ_NOEXCEPT
     {
-        this->copy_string_noexcept(in_string.data(), in_string.size());
+        this->assign_external_noexcept(
+            in_string.data(), in_string.data() + in_string.size());
     }
 
     /** @copydoc storage_base(this_type const&)
@@ -180,70 +181,6 @@ class psyq::string::_private::storage_base
     }
 
     //-------------------------------------------------------------------------
-    protected: void insert(
-        std::size_t const in_position,
-        typename this_type::traits_type::char_type const* const in_string,
-        std::size_t const in_size)
-    {
-        if (in_size <= 0 || in_string == nullptr)
-        {
-            PSYQ_ASSERT(in_size <= 0);
-        }
-        else if (
-            in_string < this->data()
-            || this->data() + this_type::MAX_SIZE < in_string)
-        {
-            this->insert_string(in_position, in_string, in_size);
-        }
-        else
-        {
-            this_type local_string;
-            local_string.copy_string(in_string, in_size);
-            this->insert_string(
-                in_position, local_string.data(), local_string.size());
-        }
-    }
-
-    private: void insert_string(
-        std::size_t const in_position,
-        typename this_type::traits_type::char_type const* const in_string,
-        std::size_t const in_size)
-    {
-        PSYQ_ASSERT(in_string != nullptr);
-        auto const local_position((std::min)(in_position, this->size()));
-        this_type::traits_type::copy(
-            &this->storage_[local_position],
-            in_string,
-            this->insert_space(local_position, in_size));
-    }
-
-    private: std::size_t insert_space(
-        std::size_t const in_position,
-        std::size_t const in_size)
-    {
-        PSYQ_ASSERT(0 < in_size);
-        auto const local_insert_size(
-            (std::min)(in_size, this_type::MAX_SIZE - in_position));
-        if (in_position < this->size())
-        {
-            auto const local_insert_end(in_position + local_insert_size);
-            if (local_insert_end < this_type::MAX_SIZE)
-            {
-                auto const local_position(&this->storage_[in_position]);
-                this_type::traits_type::move(
-                    local_position + in_size,
-                    local_position,
-                    (std::min)(
-                        this->size() - in_position,
-                        this_type::MAX_SIZE - local_insert_end));
-            }
-        }
-        this->size_ += local_insert_size;
-        this->storage_[this->size()] = 0;
-        return local_insert_size;
-    }
-
-    //-------------------------------------------------------------------------
     /// @name 文字列の変更
     //@{
     /// @brief 文字列を空にする。
@@ -252,33 +189,220 @@ class psyq::string::_private::storage_base
         this->size_ = 0;
         this->storage_[0] = 0;
     }
-    //@}
-    /// @copydoc copy_string_noexcept
-    protected: void copy_string(
-        typename traits_type::char_type const* const in_data,
-        std::size_t const in_size)
-    {
-        if (this_type::MAX_SIZE < in_size)
-        {
-            //throw std::out_of_range(__FILE__ __LINE__);
-        }
-        this->copy_string_noexcept(in_data, in_size);
-    }
-    /** @brief 文字列をコピーする。
-        @param[in] in_data コピー元となる文字列の先頭位置。
-        @param[in] in_size コピー元となる文字列の要素数。
+
+    /** @brief std::basic_string::reserve と互換性を保つためのダミー関数。
+
+        実際は何もしない。
      */
-    protected: void copy_string_noexcept(
-        typename traits_type::char_type const* const in_data,
-        std::size_t const in_size)
+    public: void reserve(std::size_t const in_capacity)
+    {
+        PSYQ_ASSERT(in_capacity <= this_type::MAX_SIZE);
+    }
+
+    /** @brief std::basic_string::shrink_to_fit と互換性を保つためのダミー関数。
+
+        実際は何もしない。
+     */
+    public: void shrink_to_fit() PSYQ_NOEXCEPT {}
+    //@}
+    /** @brief 文字を挿入する。
+        @param[in] in_offset 文字を挿入するオフセット位置。
+        @param[in] in_count  挿入する文字の数。
+        @param[in] in_char   挿入する文字。
+     */
+    protected: void insert(
+        std::size_t const in_offset,
+        std::size_t const in_count,
+        typename this_type::traits_type::char_type const in_char)
+    {
+        auto const local_gap(this->make_gap(in_offset, in_count));
+        this_type::traits_type::assign(
+            this->begin() + local_gap.first, local.gap.second, in_char);
+    }
+
+    /** @brief 文字列を挿入する。
+        @param[in] in_position 文字列を挿入する位置。
+        @param[in] in_begin    挿入する文字列の先頭位置。
+        @param[in] in_end      挿入する文字列の末尾位置。
+        @return *this
+     */
+    protected: template<typename template_iterator>
+    void insert(
+        typename this_type::traits_type::char_type* const in_position,
+        template_iterator const in_begin,
+        template_iterator const in_end)
+    {
+        if (in_begin == in_end || &(*in_begin) == nullptr)
+        {
+            PSYQ_ASSERT_THROW(
+                &(*in_begin) != nullptr, std::invalid_argument);
+        }
+        else if (this->is_included(this_type::MAX_SIZE, &(*in_begin)))
+        {
+            // *thisの部分文字列が挿入されるので、
+            // 別の文字列にコピーしてから挿入する。
+            this_type local_string;
+            local_string.assign_external(in_begin, in_end);
+            this->insert_external(
+                in_position,
+                local_string.data(),
+                local_string.data() + local_string.size());
+        }
+        else
+        {
+            this->insert_external(in_position, in_begin, in_end);
+        }
+    }
+
+    private: template<typename template_iterator>
+    void insert_external(
+        typename this_type::traits_type::char_type* const in_position,
+        template_iterator const in_begin,
+        template_iterator const in_end)
+    {
+        std::size_t local_offset;
+        if (in_position < this->begin())
+        {
+            PSYQ_ASSERT_THROW(false, std::range_error);
+            local_offset = 0;
+        }
+        else
+        {
+            local_offset = in_position - this->begin();
+            if (this->size() < local_offset)
+            {
+                PSYQ_ASSERT_THROW(false, std::range_error);
+                local_offset = this->size();
+            }
+        }
+        auto const local_gap(
+            this->make_gap(local_offset, std::distance(in_begin, in_end)));
+        this->replace_external_noexcept(
+            local_gap.first, in_begin, std::next(in_begin, local_gap.second));
+    }
+
+    /** @brief 文字列の要素を削除する。
+        @param[in] in_offset 削除を開始するオフセット位置。
+        @param[in] in_count  削除する要素数。
+     */
+    protected: void erase(
+        std::size_t const in_offset,
+        std::size_t const in_count)
+    {
+        PSYQ_ASSERT(in_offset <= this->size() && in_count <= this->size());
+        auto const local_erase_end(in_offset + in_count);
+        if (local_erase_end < this->size())
+        {
+            auto const local_position(this->begin() + in_offset);
+            this_type::traits_type::move(
+                local_position,
+                local_position + in_count,
+                this->size() - local_erase_end);
+            this->size_ -= in_count;
+        }
+        else
+        {
+            this->size_ = in_offset;
+        }
+        this->storage_[this->size()] = 0;
+    }
+
+    /** @brief 外部の文字列をコピーする。
+        @param[in] in_begin コピー元となる文字列の先頭位置。
+        @param[in] in_end   コピー元となる文字列の末尾位置。
+     */
+    protected: template<typename template_iterator>
+    void assign_external(
+        template_iterator const in_begin,
+        template_iterator const in_end)
+    {
+        if (in_begin != in_end)
+        {
+            PSYQ_ASSERT_THROW(
+                &(*in_begin) != nullptr, std::invalid_argument);
+            PSYQ_ASSERT_THROW(
+                std::distance(in_begin, in_end) <= this_type::MAX_SIZE,
+                std::length_error);
+        }
+        this->assign_external_noexcept(in_begin, in_end);
+    }
+
+    /// @copydoc assign_external
+    private: template<typename template_iterator>
+    void assign_external_noexcept(
+        template_iterator const in_begin,
+        template_iterator const in_end)
     PSYQ_NOEXCEPT
     {
-        PSYQ_ASSERT(in_size <= this_type::MAX_SIZE);
-        auto const local_size(
-            (std::min<std::size_t>)(in_size, this_type::MAX_SIZE));
-        this->size_ = local_size;
-        this_type::traits_type::copy(this->begin(), in_data, local_size);
-        this->storage_[local_size] = 0;
+        this->size_ = (std::min<std::size_t>)(
+            std::distance(in_begin, in_end), this_type::MAX_SIZE);
+        this->replace_external_noexcept(0, in_begin, in_end);
+        this->storage_[this->size()] = 0;
+    }
+
+    private: template<typename template_iterator>
+    void replace_external_noexcept(
+        std::size_t const in_offset,
+        template_iterator const in_begin,
+        template_iterator const in_end)
+    PSYQ_NOEXCEPT
+    {
+        PSYQ_ASSERT(&(*in_begin) != nullptr || in_begin != in_end);
+        PSYQ_ASSERT(
+            in_offset + std::distance(in_begin, in_end) <= this->size());
+        auto local_last(this->begin());
+        auto const local_limit(this->begin() + this_type::MAX_SIZE);
+        for (auto i(in_begin); i != in_end; ++i, ++local_last)
+        {
+            if (local_limit <= local_last)
+            {
+                PSYQ_ASSERT(false);
+                break;
+            }
+            *local_last = *i;
+        }
+    }
+
+    private: std::pair<std::size_t, std::size_t> make_gap(
+        std::size_t const in_offset,
+        std::size_t const in_size)
+    {
+        // 挿入する文字列の要素数を決定する。
+        auto const local_offset((std::min)(in_offset, this->size()));
+        PSYQ_ASSERT(in_offset == local_offset);
+        auto const local_insert_size_max(this_type::MAX_SIZE - local_offset);
+        auto const local_insert_size(
+            (std::min)(local_insert_size_max, in_size));
+        PSYQ_ASSERT_THROW(in_size == local_insert_size, std::overflow_error);
+
+        // 移動する文字列の要素数を決定する。
+        auto const local_move_size_max(
+            local_insert_size_max - local_insert_size);
+        auto const local_move_size(
+            (std::min)(local_move_size_max, this->size() - local_offset));
+        PSYQ_ASSERT_THROW(
+            local_move_size <= local_move_size_max, std::overflow_error);
+
+        // 挿入位置より後ろの文字列を移動する。
+        auto const local_position(&this->storage_[local_offset]);
+        this_type::traits_type::move(
+            local_position + local_insert_size,
+            local_position,
+            local_move_size);
+
+        // 文字列の長さを書き換える。
+        this->size_ += local_insert_size;
+        this->storage_[this->size()] = 0;
+        return std::make_pair(local_offset, local_insert_size);
+    }
+
+    private: bool is_included(
+        std::size_t const in_size,
+        void const* const in_string)
+    const PSYQ_NOEXCEPT
+    {
+        return this->data() <= in_string
+            && in_string <= this->data() + in_size;
     }
 
     //-------------------------------------------------------------------------
@@ -339,7 +463,7 @@ public psyq::string::_private::interface_mutable<
     public: storage(typename base_type::view const& in_string):
     base_type(base_type::string_type::make())
     {
-        this->copy_string(in_string.data(), in_string.size());
+        this->assign_external(in_string.begin(), in_string.end());
     }
 
     /** @brief 文字列をコピーして構築する。
@@ -354,8 +478,8 @@ public psyq::string::_private::interface_mutable<
     :
     base_type(base_type::string_type::make())
     {
-        this->copy_string(
-            in_data, base_type::adjust_size(in_data, in_size));
+        this->assign_external(
+            in_data, in_data + base_type::adjust_size(in_data, in_size));
     }
 
     /** @brief 文字列をコピーして構築する。
@@ -369,7 +493,7 @@ public psyq::string::_private::interface_mutable<
     :
     base_type(base_type::string_type::make())
     {
-        this->copy_string(&(*in_begin), std::distance(in_begin, in_end));
+        this->assign_external(in_begin, in_end);
     }
 
     /** @brief 文字列の一部をコピーして構築する。
@@ -384,9 +508,8 @@ public psyq::string::_private::interface_mutable<
     :
     base_type(base_type::string_type::make())
     {
-        typename base_type::view const local_string(
-            in_string, in_offset, in_count);
-        this->copy_string(local_string.data(), local_string.size());
+        auto const local_substr(in_string.substr(in_offset, in_count));
+        this->assign_external(local_substr.begin(), local_substr.end());
     }
 
     /** @brief 文字を繰り返す文字列を構築する。
@@ -399,11 +522,7 @@ public psyq::string::_private::interface_mutable<
     :
     base_type(base_type::string_type::make())
     {
-        PSYQ_ASSERT(in_count < this_type::MAX_SIZE);
-        auto const local_count((std::min)(in_count, this_type::MAX_SIZE));
-        base_type::traits_type::assign(this->begin(), local_count, in_char);
-        this->storage_[local_count] = 0;
-        this->size_ = local_count;
+        this->insert(0, in_count, in_char);
     }
     //@}
     //-------------------------------------------------------------------------
